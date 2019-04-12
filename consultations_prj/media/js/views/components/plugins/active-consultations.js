d5d216438e5c94547c1151e2e9bb3f151eb59767
Alexei Peters <apeters@fargeo.com>
2019-04-12 14:58:17 -0700

    add all elements of the active consultations view, re #13

--- consultations_prj/media/js/views/components/plugins/active-consultations.js
+++ consultations_prj/media/js/views/components/plugins/active-consultations.js
@@ -1,14 +1,20 @@
 define([
-    'knockout'
+    'knockout',
+    'bindings/chosen'
 ], function(ko) {
     return ko.components.register('active-consultations', {
         viewModel: function(params) {
+            var self = this;
             this.title = 'Active Consultations';
+            this.layout = ko.observable('grid');
+            this.setLayout = function(layout){
+                self.layout(layout);
+            };
 
             this.active_items = [
-                {title: '34 Victoria Street, Westminster', description: 'Consultation/Proposal description, limited to just the first few lines in the consultation so that users can quickly scan and see if it is the consultation they are looking for', author: 'Sarah Harrison'},
-                {title: '18 Minster Yard, Kensington', description: 'Consultation/Proposal description, limited to just the first few lines in the consultation so that users can quickly scan and see if it is the consultation they are looking for', author: 'Laura O\'Gorman'},
-                {title: 'Bishops Palace, East Hall', description: 'Another description here', author: 'Stewart Cakebread'}
+                {title: '34 Victoria Street, Westminster', description: 'Consultation/Proposal description, limited to just the first few lines in the consultation so that users can quickly scan and see if it is the consultation they are looking for', author: 'Sarah Harrison', consultation_type: 'Planning application - minor'},
+                {title: '18 Minster Yard, Kensington', description: 'Consultation/Proposal description, limited to just the first few lines in the consultation so that users can quickly scan and see if it is the consultation they are looking for', author: 'Laura O\'Gorman', consultation_type: 'Planning application - major'},
+                {title: 'Bishops Palace, East Hall', description: 'Another description here', author: 'Stewart Cakebread', consultation_type: 'Planning application - major'}
             ];
             
 
