013975ef61c16a344f22b29289d2ddb2e0f4a2b3
Alexei Peters <apeters@fargeo.com>
2019-04-12 16:21:43 -0700

    more work on active consultations view, re #13

--- consultations_prj/media/js/views/components/plugins/active-consultations.js
+++ consultations_prj/media/js/views/components/plugins/active-consultations.js
@@ -16,6 +16,39 @@ define([
                 {title: '18 Minster Yard, Kensington', description: 'Consultation/Proposal description, limited to just the first few lines in the consultation so that users can quickly scan and see if it is the consultation they are looking for', author: 'Laura O\'Gorman', consultation_type: 'Planning application - major'},
                 {title: 'Bishops Palace, East Hall', description: 'Another description here', author: 'Stewart Cakebread', consultation_type: 'Planning application - major'}
             ];
+
+
+            // this.doQuery = function() {
+            //     var queryString = this.queryString();
+            //     queryString.page = this.viewModel.searchResults.page();
+            //     if (this.updateRequest) {
+            //         this.updateRequest.abort();
+            //     }
+
+            //     this.viewModel.loading(true);
+
+            //     this.updateRequest = $.ajax({
+            //         type: "GET",
+            //         url: arches.urls.search_results,
+            //         data: queryString,
+            //         context: this,
+            //         success: function(response) {
+            //             var data = this.viewModel.searchResults.updateResults(response);
+            //             this.viewModel.alert(false);
+            //         },
+            //         error: function(response, status, error) {
+            //             if(this.updateRequest.statusText !== 'abort'){
+            //                 this.viewModel.alert(new AlertViewModel('ep-alert-red', arches.requestFailed.title, response.responseText));
+            //             }
+            //         },
+            //         complete: function(request, status) {
+            //             this.viewModel.loading(false);
+            //             this.updateRequest = undefined;
+            //             window.history.pushState({}, '', '?' + $.param(queryString).split('+').join('%20'));
+            //         }
+            //     });
+            // }
+
             
 
         },
