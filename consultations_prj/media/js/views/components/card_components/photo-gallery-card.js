15515e93bc3f0c4dfb18ba49c1f9188d4c26c67e
Cyrus Hiatt <chiatt@fargeo.com>
2019-04-12 17:36:00 -0700

    Adds main display photo and adds tile thumbnail gallery. re #14

--- consultations_prj/media/js/views/components/card_components/photo-gallery-card.js
+++ consultations_prj/media/js/views/components/card_components/photo-gallery-card.js
@@ -1,24 +1,92 @@
 define([
     'knockout',
     'underscore',
-    'viewmodels/card-component'
-], function(ko, _, CardComponentViewModel) {
+    'viewmodels/card-component',
+    'viewmodels/photo-gallery'
+], function(ko, _, CardComponentViewModel, PhotoGallery) {
     return ko.components.register('photo-gallery-card', {
         viewModel: function(params) {
             params.configKeys = ['acceptedFiles', 'maxFilesize'];
             var self = this;
             CardComponentViewModel.apply(this, [params]);
 
+            this.photoGallery = new PhotoGallery();
+
+            this.card.tiles().forEach(function(tile){
+                var url
+                var getUrl = function(){
+                    _.each(tile.data,
+                        function(v, k) {
+                            val = ko.unwrap(v);
+                            if (Array.isArray(val)
+                                && val.length == 1
+                                && (ko.unwrap(val[0].url) || ko.unwrap(val[0].content))) {
+                                    url = ko.unwrap(val[0].url) || ko.unwrap(val[0].content);
+                                }
+                            });
+                        return url
+                    };
+                _.extend(tile, {getUrl: getUrl})
+            })
+
+            // getUrl should instead be a function that takes a tile and returns it's url
+
+            this.displayContent = ko.pureComputed(function(){
+                var photo;
+                var selected = this.card.tiles().find(
+                    function(tile){
+                        return tile.selected() === true
+                    });
+                if (selected) {
+                        photo = selected.getUrl();
+                    }
+                return photo;
+            }, this);
+
+            if (!this.displayContent()) {
+                if (this.card.tiles().length > 0) {
+                    this.card.tiles()[0].selected(true)
+                }
+            }
+
+
+            this.dropzoneOptions = {
+                url: "arches.urls.root",
+                dictDefaultMessage: '',
+                autoProcessQueue: false,
+                autoQueue: false,
+                previewsContainer: '#hidden-dz-previews',
+                init: function() {
+                    self.dropzone = this;
+                    this.on("addedfile", function(file) {
+                        var newtile;
+                        newtile = self.card.getNewTile();
+                        newtile.data['e60af863-5d48-11e9-b44f-c4b301baab9f'](file)
+                        self.form.saveTile(newtile);
+                    }, self);
+
+                    this.on("error", function(file, error) {
+                        file.error = error;
+                        console.log(error);
+                    });
+                }
+            };
+
+
+
+
             this.tabItems = [
                 {'name': 'alpha', 'icon': 'fa fa-android'},
                 {'name': 'beta', 'icon': 'fa fa-anchor'},
                 {'name': 'delta', 'icon': 'fa fa-camera'}
             ];
+
             this.activeTab = ko.observable();
             this.setActiveTab = function(tabname){
                 var name = this.activeTab() === tabname ? '' : tabname;
                 this.activeTab(name);
             };
+
         },
         template: {
             require: 'text!templates/views/components/card_components/photo-gallery-card.htm'
