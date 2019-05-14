e5c6eb3fa84723f1a401f57d68b9661b5bf0d4c1
Galen Mancino <whatisgalen@gmail.com>
2019-05-14 14:09:54 -0700

    moves some steps out of consultation workflow (to move to Application Area), re #34

--- consultations_prj/media/js/views/components/plugins/add-consultation.js
+++ consultations_prj/media/js/views/components/plugins/add-consultation.js
@@ -11,39 +11,6 @@ define([
             var self = this;
             params.steps = [
                 {
-                    title: 'Assign Address',
-                    description: 'Assign an address to your application area. Use the address as the default name',
-                    component: 'get-tile-value',
-                    graphid: '336d34e3-53c3-11e9-ba5f-dca90488358a',
-                    nodegroupid: 'e857704a-53d8-11e9-b05a-dca90488358a',
-                    resourceid: null,
-                    tileid: null,
-                    parenttileid: null,
-                    icon: 'fa-envelope'
-                },
-                {
-                    title: 'Assign Name',
-                    description: 'Assign a name to your application area',
-                    component: 'set-tile-value',
-                    graphid: '336d34e3-53c3-11e9-ba5f-dca90488358a',
-                    nodegroupid: 'c5f909b5-53c7-11e9-a3ac-dca90488358a',
-                    resourceid: null,
-                    tileid: null,
-                    parenttileid: null,
-                    icon: 'fa-tag'
-                },
-                {
-                    title: 'Area Map',
-                    description: 'Draw (or select from the Development Area Overlay) the extent of...',
-                    component: 'new-tile-step',
-                    graphid: '336d34e3-53c3-11e9-ba5f-dca90488358a',
-                    nodegroupid: 'eafced66-53d8-11e9-a4e2-dca90488358a',
-                    resourceid: null,
-                    tileid: null,
-                    parenttileid: null,
-                    icon: 'fa-map-marker'
-                },
-                {
                     title: 'Related Heritage Resources',
                     description: 'Click on the Heritage Assets, Activities, and other resources related to this',
                     component: 'new-tile-step',
@@ -53,28 +20,6 @@ define([
                     tileid: null,
                     parenttileid: null,
                     icon: 'fa-code-fork'
-                },
-                {
-                    title: 'Area Description',
-                    description: 'Describe the Application Area',
-                    component: 'new-tile-step',
-                    graphid: '336d34e3-53c3-11e9-ba5f-dca90488358a',
-                    nodegroupid: '63cdcf0f-53da-11e9-8340-dca90488358a',
-                    resourceid: null,
-                    tileid: null,
-                    parenttileid: null,
-                    icon: 'fa-clipboard'
-                },
-                {
-                    title: 'Area Designations',
-                    description: 'Select the Application Area designations',
-                    component: 'new-tile-step',
-                    graphid: '336d34e3-53c3-11e9-ba5f-dca90488358a',
-                    nodegroupid: 'e19fe3a1-6d22-11e9-98bf-dca90488358a',
-                    resourceid: null,
-                    tileid: null,
-                    parenttileid: null,
-                    icon: 'fa-bookmark'
                 },
                 {
                     title: 'Application Details',
