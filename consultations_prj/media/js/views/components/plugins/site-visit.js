01954d157e6c6562dfb9b2af32e204d7c5594840
Galen Mancino <whatisgalen@gmail.com>
2019-06-14 14:51:20 -0700

    adds site-visit related files, re #48

--- consultations_prj/media/js/views/components/plugins/site-visit.js
+++ consultations_prj/media/js/views/components/plugins/site-visit.js
@@ -25,55 +25,81 @@ define([
                     nameheading: 'New Site Visit',
                     namelabel: '[no label]'
                 },
-                // {
-                //     title: 'Assign Name',
-                //     name: 'setname',
-                //     description: 'Assign a name to your application area',
-                //     component: 'views/components/workflows/set-tile-value',
-                //     componentname: 'set-tile-value',
-                //     graphid: '336d34e3-53c3-11e9-ba5f-dca90488358a',
-                //     nodegroupid: 'c5f909b5-53c7-11e9-a3ac-dca90488358a',
-                //     resourceid: null,
-                //     tileid: null,
-                //     parenttileid: null,
-                //     icon: 'fa-tag'
-                // },
-                // {
-                //     title: 'Area Map',
-                //     description: 'Draw (or select from the Development Area Overlay) the extent of...',
-                //     component: 'views/components/workflows/new-tile-step',
-                //     componentname: 'new-tile-step',
-                //     graphid: '336d34e3-53c3-11e9-ba5f-dca90488358a',
-                //     nodegroupid: 'eafced66-53d8-11e9-a4e2-dca90488358a',
-                //     resourceid: null,
-                //     tileid: null,
-                //     parenttileid: null,
-                //     icon: 'fa-map-marker'
-                // },
-                // {
-                //     title: 'Area Description',
-                //     description: 'Describe the Application Area',
-                //     component: 'views/components/workflows/new-tile-step',
-                //     componentname: 'new-tile-step',
-                //     graphid: '336d34e3-53c3-11e9-ba5f-dca90488358a',
-                //     nodegroupid: '63cdcf0f-53da-11e9-8340-dca90488358a',
-                //     resourceid: null,
-                //     tileid: null,
-                //     parenttileid: null,
-                //     icon: 'fa-clipboard'
-                // },
-                // {
-                //     title: 'Area Designations',
-                //     description: 'Select the Application Area designations',
-                //     component: 'views/components/workflows/new-tile-step',
-                //     componentname: 'new-tile-step',
-                //     graphid: '336d34e3-53c3-11e9-ba5f-dca90488358a',
-                //     nodegroupid: 'e19fe3a1-6d22-11e9-98bf-dca90488358a',
-                //     resourceid: null,
-                //     tileid: null,
-                //     parenttileid: null,
-                //     icon: 'fa-bookmark'
-                // }
+                {
+                    title: 'Site Location',
+                    name: 'sitelocation',
+                    description: '',
+                    component: 'views/components/workflows/new-tile-value',
+                    componentname: 'new-tile-value',
+                    graphid: '0a06b0ee-6c46-11e9-abff-dca90488358a',
+                    nodegroupid: '571dba97-6c47-11e9-8bed-dca90488358a',
+                    resourceid: null,
+                    tileid: null,
+                    parenttileid: null,
+                    icon: 'fa-map-marker',
+                    nameheading: 'New Site Visit',
+                    namelabel: '[no label]'
+                },
+                {
+                    title: 'Site Visit Attendees',
+                    name: 'siteattendees',
+                    description: '',
+                    component: 'views/ocmponents/workflows/new-tile-value',
+                    componentname: 'new-tile-value',
+                    graphid: '0a06b0ee-6c46-11e9-abff-dca90488358a',
+                    nodegroupid: 'f57ec302-6c46-11e9-93b3-dca90488358a',
+                    resourceid: null,
+                    tileid: null,
+                    parenttileid: null,
+                    icon: 'fa-user-plus',
+                    nameheading: 'New Site Visit',
+                    namelabel: '[no label]'
+                },
+                {
+                    title: 'Site Visit Observations',
+                    name: 'siteobservations',
+                    description: '',
+                    component: 'views/ocmponents/workflows/new-tile-value',
+                    componentname: 'new-tile-value',
+                    graphid: '0a06b0ee-6c46-11e9-abff-dca90488358a',
+                    nodegroupid: 'b21ef6c0-6c46-11e9-9205-dca90488358a',
+                    resourceid: null,
+                    tileid: null,
+                    parenttileid: null,
+                    icon: 'fa-lightbulb-o',
+                    nameheading: 'New Site Visit',
+                    namelabel: '[no label]'
+                },
+                {
+                    title: 'Site Visit Recommendations',
+                    name: 'siterecommendations',
+                    description: '',
+                    component: 'views/ocmponents/workflows/new-tile-value',
+                    componentname: 'new-tile-value',
+                    graphid: '0a06b0ee-6c46-11e9-abff-dca90488358a',
+                    nodegroupid: 'cb717640-6c46-11e9-9b10-dca90488358a',
+                    resourceid: null,
+                    tileid: null,
+                    parenttileid: null,
+                    icon: 'fa-clipboard',
+                    nameheading: 'New Site Visit',
+                    namelabel: '[no label]'
+                },
+                {
+                    title: 'Site Photos',
+                    name: 'sitephotos',
+                    description: '',
+                    component: 'views/ocmponents/workflows/final-step',
+                    componentname: 'final-step',
+                    graphid: '0a06b0ee-6c46-11e9-abff-dca90488358a',
+                    nodegroupid: '4451d38f-6c47-11e9-83e0-dca90488358a',
+                    resourceid: null,
+                    tileid: null,
+                    parenttileid: null,
+                    icon: 'fa-camera',
+                    nameheading: 'New Site Visit',
+                    namelabel: '[no label]'
+                }
             ];
 
             Workflow.apply(this, [params]);
@@ -87,21 +113,21 @@ define([
                     self.state.previousstep = previousStep._index;
                     self.updateUrl();
                 }
-                if (ko.unwrap(activeStep.name) === 'assignaddress') {
-                    activeStep.requirements = self.state.steps.assignaddress;
-                }
-                if (ko.unwrap(activeStep.name) === 'setname') {
-                    if (self.state.steps['assignaddress']) {
-                        var tiledata = self.state.steps['assignaddress'].tile
-                        var tilevals = _.map(tiledata, function(v, k) {return v})
-                        var nodeval = tilevals[0] + "," + tilevals[1] + " " + tilevals[2];
-                        activeStep.requirements = self.state.steps.setname || {};
-                        activeStep.requirements.applyOutputToTarget = self.state.steps['assignaddress'].applyOutputToTarget;
-                        activeStep.requirements.targetnode = '1b95fb70-53ef-11e9-9001-dca90488358a';
-                        activeStep.requirements.targetnodegroup = ko.unwrap(activeStep.nodegroupid);
-                        activeStep.requirements.value = nodeval;
-                    }
-                }
+                // if (ko.unwrap(activeStep.name) === 'assignaddress') {
+                //     activeStep.requirements = self.state.steps.assignaddress;
+                // }
+                // if (ko.unwrap(activeStep.name) === 'setname') {
+                //     if (self.state.steps['assignaddress']) {
+                //         var tiledata = self.state.steps['assignaddress'].tile
+                //         var tilevals = _.map(tiledata, function(v, k) {return v})
+                //         var nodeval = tilevals[0] + "," + tilevals[1] + " " + tilevals[2];
+                //         activeStep.requirements = self.state.steps.setname || {};
+                //         // activeStep.requirements.applyOutputToTarget = self.state.steps['assignaddress'].applyOutputToTarget;
+                //         // activeStep.requirements.targetnode = '1b95fb70-53ef-11e9-9001-dca90488358a';
+                //         activeStep.requirements.targetnodegroup = ko.unwrap(activeStep.nodegroupid);
+                //         activeStep.requirements.value = nodeval;
+                //     }
+                // }
                 self.previousStep(val);
             }
 
@@ -111,6 +137,6 @@ define([
 
             self.ready(true);
         },
-        template: { require: 'text!templates/views/components/plugins/application-area.htm' }
+        template: { require: 'text!templates/views/components/plugins/site-visit.htm' }
     });
 });
