967a67596d45b663be274bd170cdca394fc0a109
Galen Mancino <whatisgalen@gmail.com>
2019-07-25 11:42:22 -0700

    adds additional, separate url for the django view for activeconsultations, re #93

--- consultations_prj/media/js/views/components/plugins/active-consultations.js
+++ consultations_prj/media/js/views/components/plugins/active-consultations.js
@@ -54,6 +54,27 @@ define([
                 },
             });
 
+            this.hitView = function() {
+                $.ajax({
+                    type: "GET",
+                    url: arches.urls.root + 'filetemplate',
+                    data: {
+                        "resourceinstance_id": tile["resourceinstance_id"], //instance of Communication
+                        "template_id": tile["data"]["23e1ac91-6c4b-11e9-8641-dca90488358a"]
+                    },
+                    context: self,
+                    success: function(responseText, status, response){
+                        console.log(response.responseJSON);
+                    },
+                    error: function(response, status, error) {
+                        console.log(response);
+                        if(response.statusText !== 'abort'){
+                            this.viewModel.alert(new AlertViewModel('ep-alert-red', arches.requestFailed.title, response.responseText));
+                        }
+                    }
+                });
+            }
+
             this.setNodeIds = function() {
                 ko.utils.arrayForEach(self.active_items(), function(item) {
                     if(item["_consultation"]) {
