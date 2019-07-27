0b05e5553f51344bf7be3ffcf579779c7cd8b993
Galen Mancino <whatisgalen@gmail.com>
2019-07-26 18:11:25 -0700

    cleans up unused code in vm, re #93

--- consultations_prj/media/js/views/components/plugins/active-consultations.js
+++ consultations_prj/media/js/views/components/plugins/active-consultations.js
@@ -8,13 +8,10 @@ define([
     return ko.components.register('active-consultations',  {
         viewModel: function(params) {
             var self = this;
-            this.dataReady = ko.observable(false);
-            // this.title = 'Active Consultations';
             this.layout = ko.observable('grid');
             this.setLayout = function(layout){
                 self.layout(layout);
             };
-            // this.mapData = ko.observable();
             this.loading = ko.observable(true);
             this.mapImageURL = ko.observable('');
 
@@ -25,13 +22,7 @@ define([
                     data["mapImageUrl"](map.getCanvas().toDataURL("image/jpeg"));
                 });
                 $("#map").remove();
-
             };
-            this.queryString = {
-                "advanced-search": [
-                    {"op":"and","8d41e4dd-a250-11e9-9032-00224800b26d":{"op":"","val":""},"8d41e4d5-a250-11e9-b968-00224800b26d":{"op":"","val":""},"8d41e4cc-a250-11e9-87b3-00224800b26d":{"op":"","val":""},"8d41e4d3-a250-11e9-8977-00224800b26d":{"op":"!","val":"149f3488-70ba-4ec0-aa49-d37bf879d133"}},{"op":"and","8d41e4dd-a250-11e9-9032-00224800b26d":{"op":"","val":""},"8d41e4d5-a250-11e9-b968-00224800b26d":{"op":"","val":""},"8d41e4cc-a250-11e9-87b3-00224800b26d":{"op":"","val":""},"8d41e4d3-a250-11e9-8977-00224800b26d":{"op":"!","val":"7b3c5855-10f4-4b7f-807b-b68397535613"}}
-                ]
-            }
 
             this.getConsultations = $.ajax({
                 type: "GET",
@@ -40,7 +31,6 @@ define([
                 context: self,
                 success: function(responseText, status, response){
                     Object.entries(response.responseJSON['tile_dict']).forEach( function(pair) {
-                        console.log(pair[1]);
                         if(pair[1]['Consultation Type'] == undefined) {
                             pair[1]['Consultation Type'] = '';
                         }
@@ -52,7 +42,6 @@ define([
                         }
                         self.active_items.push(pair[1]);
                     });
-                    console.log(response.responseJSON);
                     self.loading(false);
                 },
                 error: function(response, status, error) {
@@ -62,7 +51,6 @@ define([
                     }
                 }
             });
-
 
 
             // this.doQuery = function() {
@@ -95,8 +83,6 @@ define([
             //         }
             //     });
             // }
-
-
 
         },
         template: { require: 'text!templates/views/components/plugins/active-consultations.htm' }
