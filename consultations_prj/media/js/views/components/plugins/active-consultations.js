fe152d171959057b7df61094b93bc8eb8b9d4c57
Galen Mancino <whatisgalen@gmail.com>
2019-07-31 19:53:56 -0700

    cleans up whitespace in active-consultations vm, re #93

--- consultations_prj/media/js/views/components/plugins/active-consultations.js
+++ consultations_prj/media/js/views/components/plugins/active-consultations.js
@@ -9,14 +9,11 @@ define([
         viewModel: function(params) {
             var self = this;
             this.layout = ko.observable('grid');
-            this.setLayout = function(layout){
-                self.layout(layout);
-            };
+            this.setLayout = function(layout){ self.layout(layout); };
             this.loading = ko.observable(true);
             this.mapImageURL = ko.observable('');
-
             this.active_items = ko.observableArray([]);
-
+            
             this.setupMap = function(map, data) {
                 map.on('load', function() {
                     data["mapImageUrl"](map.getCanvas().toDataURL("image/jpeg"));
@@ -31,7 +28,6 @@ define([
                 context: self,
                 success: function(responseText, status, response){
                     Object.entries(response.responseJSON['tile_dict']).forEach( function(keyPair) {
-                        console.log(keyPair);
                         keyPair[1]["mapImageUrl"] = ko.observable(false);
                         keyPair[1]["zoom"] = 15;
                         if(keyPair[1]['Consultation Type'] == undefined) { keyPair[1]['Consultation Type'] = ''; }
@@ -39,14 +35,11 @@ define([
                             keyPair[1]["Geospatial Location"] = {"features": [{"geometry":{"coordinates":[0,0]}}]};
                             keyPair[1]["zoom"] = 0;
                         }
-                        console.log(keyPair[1]["Geospatial Location"]);
                         if(typeof keyPair[1]["Geospatial Location"]["features"][0]["geometry"]["coordinates"][0] != "number") {
                             keyPair[1]["center"] = keyPair[1]["Geospatial Location"]["features"][0]["geometry"]["coordinates"][0][0];
                         } else {
                             keyPair[1]["center"] = keyPair[1]["Geospatial Location"]["features"][0]["geometry"]["coordinates"];
                         }
-                        
-                        // no area defined, greyed out world with
                         self.active_items.push(keyPair[1]);
                     });
                     self.loading(false);
@@ -57,7 +50,6 @@ define([
                     }
                 }
             });
-
         },
         template: { require: 'text!templates/views/components/plugins/active-consultations.htm' }
     });
