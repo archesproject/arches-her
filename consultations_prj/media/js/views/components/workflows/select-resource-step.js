e2c4e1c1982e21ef44329089530865819860c906
Galen Mancino <whatisgalen@gmail.com>
2019-08-01 11:15:10 -0700

    adds a card component lookup to the template for select-resource-step, re #90

--- consultations_prj/media/js/views/components/workflows/select-resource-step.js
+++ consultations_prj/media/js/views/components/workflows/select-resource-step.js
@@ -1,114 +1,93 @@
 define([
+    'underscore',
+    'jquery',
+    'arches',
     'knockout',
-    'views/components/workflows/new-tile-step',
-    'viewmodels/alert'
-], function(ko, NewTileStepViewModel, AlertViewModel) {
+    'knockout-mapping',
+    'views/components/workflows/new-tile-step'
+], function(_, $, arches, ko, koMapping, NewTileStep) {
+    function viewModel(params) {
         var self = this;
-        console.log(params);
+        NewTileStep.apply(this, [params]);
+        params.applyOutputToTarget = ko.observable(true);
         if (!params.resourceid() && params.requirements){
-            // console.log('whats this?',params.requirements.resourceid);
             params.resourceid(params.requirements.resourceid);
             params.tileid(params.requirements.tileid);
         }
-        var url = arches.urls.api_card + (ko.unwrap(params.resourceid) || ko.unwrap(params.graphid));
-
-        // this.card = ko.observable();
-        // this.tile = ko.observable();
+        this.nameheading = params.nameheading;
+        this.namelabel = params.namelabel;
         this.resValue = ko.observable();
-        this.loading = params.loading || ko.observable(false);
-        this.alert = params.alert || ko.observable(null);
-        this.resourceId = params.resourceid || ko.observable();
-        this.complete = params.complete || ko.observable();
-        this.completeOnSave = params.completeOnSave === false ? false : true;
-        this.loading(true);
+        this.applyOutputToTarget = params.applyOutputToTarget;
 
-        $.getJSON(url, function(data) {
-            var handlers = {
-                'after-update': [],
-                'tile-reset': []
-            };
-            var displayname = ko.observable(data.displayname);
-            // var createLookup = function(list, idKey) {
-            //     return _.reduce(list, function(lookup, item) {
-            //         lookup[item[idKey]] = item;
-            //         return lookup;
-            //     }, {});
-            // };
-            // var flattenTree = function(parents, flatList) {
-            //     _.each(ko.unwrap(parents), function(parent) {
-            //         flatList.push(parent);
-            //         var childrenKey = parent.tiles ? 'tiles' : 'cards';
-            //         flattenTree(
-            //             ko.unwrap(parent[childrenKey]),
-            //             flatList
-            //         );
-            //     });
-            //     return flatList;
-            // };
+        this.workflowStepClass = ko.pureComputed(function() {
+            return self.applyOutputToTarget() ? params.class() : '';
+        }, viewModel);
 
-            self.reviewer = data.userisreviewer;
-            // self.provisionalTileViewModel = new ProvisionalTileViewModel({
-            //     tile: self.tile,
-            //     reviewer: data.userisreviewer
-            // });
+        params.tile = self.tile;
 
-            var graphModel = new GraphModel({
-                data: {
-                    nodes: data.nodes,
-                    nodegroups: data.nodegroups,
-                    edges: []
-                },
-                datatypes: data.datatypes
-            });
-
-            self.on = function(eventName, handler) {
-                if (handlers[eventName]) {
-                    handlers[eventName].push(handler);
+        params.stateProperties = function(){
+                return {
+                    resourceid: ko.unwrap(params.resourceid),
+                    tile: !!(ko.unwrap(params.tile)) ? koMapping.toJS(params.tile().data) : undefined,
+                    tileid: !!(ko.unwrap(params.tile)) ? ko.unwrap(params.tile().tileid): undefined,
+                    applyOutputToTarget: ko.unwrap(this.applyOutputToTarget)
                 }
             };
 
-            self.loading(false);
-            // commented the line below because it causes steps to automatically advance on page reload
-            // self.complete(!!ko.unwrap(params.tileid));
+        self.updateTargetTile = function(tiles){
+            var targetresult;
+            var targettile;
+            var sourcetile;
+            var targetvals;
+            tiles.forEach(function(tile){
+                    if (tile.nodegroup_id === ko.unwrap(params.targetnodegroup)) {
+                        targettile = tile;
+                    } else if (tile.nodegroup_id === ko.unwrap(params.nodegroupid)) {
+                        sourcetile = tile;
+                    }
+                });
+            targetvals = _.map(sourcetile.data, function(v, k) {return ko.unwrap(v)})
+            targetresult = targetvals[2] + ", " + targetvals[0] + " " + targetvals[1];
+            targettile.data[params.targetnode()](targetresult);
+            targettile.save();
+        };
+
+        self.applyOutputToTarget.subscribe(function(val){
+            if (val && self.tiles && self.tiles.length > 0) {
+                self.updateTargetTile(self.tiles);
+            }
         });
-        // params.tile = self.tile;
-        // params.stateProperties = function(){
-        //     return {
-        //         resourceid: ko.unwrap(params.resourceid),
-        //         tile: !!(ko.unwrap(params.tile)) ? koMapping.toJS(params.tile().data) : undefined,
-        //         tileid: !!(ko.unwrap(params.tile)) ? ko.unwrap(params.tile().tileid): undefined
-        //     };
-        // };
-        
 
         self.setResourceInstance = function() {
             console.log(params);
             if (self.resValue() != null) {
                 console.log(self.resValue());
-                params.resourceid(self.resValue());
-                self.complete(true);
+                params.requirements.resourceid(self.resValue());
+                // self.complete(true);
             }
         }
 
-        // self.onSaveSuccess = function(tiles) {
-        //     var tile;
-        //     if (tiles.length > 0) {
-        //         tile = tiles[0];
-        //         params.resourceid(tile.resourceinstance_id);
-        //         params.tileid(tile.tileid);
-        //         self.resourceId(tile.resourceinstance_id);
-        //     }
-        //     if (self.completeOnSave === true) {
-        //         self.complete(true);
-        //     }
-        // };
+        self.onSaveSuccess = function(tiles) {
+            self.tiles = tiles;
+            if (self.tiles.length > 0) {
+                params.resourceid(tiles[0].resourceinstance_id);
+                self.resourceId(tiles[0].resourceinstance_id);
+            }
+            if (self.applyOutputToTarget()) {
+                self.updateTargetTile(tiles)
+            }
+            if (self.completeOnSave === true) {
+                self.complete(true);
+            }
+        }
+    };
 
-    
-    ko.components.register('select-resource-step', {
+    return ko.components.register('select-resource-step', {
         viewModel: viewModel,
         template: {
             require: 'text!templates/views/components/workflows/select-resource-step.htm'
         }
     });
+
     return viewModel;
 });
