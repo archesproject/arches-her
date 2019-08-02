08d44e3262e9b7e4d45b1423e1ebf22757ef49a3
Galen Mancino <whatisgalen@gmail.com>
2019-08-01 18:07:28 -0700

    changes nodegroup from name to visit date, re #90

--- consultations_prj/media/js/views/components/workflows/select-resource-step.js
+++ consultations_prj/media/js/views/components/workflows/select-resource-step.js
@@ -18,10 +18,12 @@ define([
         this.namelabel = params.namelabel;
         this.resValue = ko.observable();
         this.applyOutputToTarget = params.applyOutputToTarget;
-
-        // this.workflowStepClass = ko.pureComputed(function() {
-        //     return self.applyOutputToTarget() ? params.class() : '';
-        // }, viewModel);
+        this.resValue.subscribe(function(val){
+            params.requirements.resourceid = ko.unwrap(val);
+            params.resourceid(ko.unwrap(val));
+            console.log(params.resourceid());
+        }, this);
+        console.log(params);
 
         params.tile = self.tile;
 
@@ -30,51 +32,14 @@ define([
                 resourceid: ko.unwrap(params.resourceid),
                 tile: !!(ko.unwrap(params.tile)) ? koMapping.toJS(params.tile().data) : undefined,
                 tileid: !!(ko.unwrap(params.tile)) ? ko.unwrap(params.tile().tileid): undefined,
-                applyOutputToTarget: ko.unwrap(this.applyOutputToTarget)
             }
         };
-        console.log(params);
-
-        self.updateTargetTile = function(tiles){
-            var targetresult;
-            var targettile;
-            var sourcetile;
-            var targetvals;
-            tiles.forEach(function(tile){
-                    if (tile.nodegroup_id === ko.unwrap(params.targetnodegroup)) {
-                        targettile = tile;
-                    } else if (tile.nodegroup_id === ko.unwrap(params.nodegroupid)) {
-                        sourcetile = tile;
-                    }
-                });
-            targetvals = _.map(sourcetile.data, function(v, k) {return ko.unwrap(v)})
-            targetresult = targetvals[2] + ", " + targetvals[0] + " " + targetvals[1];
-            targettile.data[params.targetnode()](targetresult);
-            targettile.save();
-        };
-
-        self.applyOutputToTarget.subscribe(function(val){
-            if (val && self.tiles && self.tiles.length > 0) {
-                self.updateTargetTile(self.tiles);
-            }
-        });
-
-        self.setResourceInstance = function() {
-            console.log(params);
-            if (self.resValue() != null) {
-                console.log(self.resValue());
-                params.requirements.resourceid = self.resValue();
-            }
-        }
 
         self.onSaveSuccess = function(tiles) {
             self.tiles = tiles;
             if (self.tiles.length > 0) {
                 params.resourceid(tiles[0].resourceinstance_id);
                 self.resourceId(tiles[0].resourceinstance_id);
-            }
-            if (self.applyOutputToTarget()) {
-                self.updateTargetTile(tiles)
             }
             if (self.completeOnSave === true) {
                 self.complete(true);
