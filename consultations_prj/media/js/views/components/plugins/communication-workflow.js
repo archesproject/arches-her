4fb946038ff90b77d4f9828cc1dfe7009dd19fc5
Galen Mancino <whatisgalen@gmail.com>
2019-08-13 15:51:24 -0700

    adds select-resource-step as component for first workflow steps in both correspondence and communication workflows, re #49

--- consultations_prj/media/js/views/components/plugins/communication-workflow.js
+++ consultations_prj/media/js/views/components/plugins/communication-workflow.js
@@ -4,27 +4,28 @@ define([
     'viewmodels/workflow-step',
     'views/components/workflows/new-tile-step',
     'views/components/workflows/set-tile-value',
-    'views/components/workflows/get-tile-value'
+    'views/components/workflows/get-tile-value',
+    'views/components/workflows/select-resource-step'
 ], function(ko, Workflow, Step) {
     return ko.components.register('communication-workflow', {
         viewModel: function(params) {
             var self = this;
             params.steps = [
-                // { //here need a widget just to select which an instance of consultation
-                //     title: 'Related Consultation',
-                //     name: 'relatedconsultation',
-                //     description: '',
-                //     component: 'views/components/workflows/new-tile-step',
-                //     componentname: 'new-tile-step',
-                //     graphid: '8d41e49e-a250-11e9-9eab-00224800b26d',
-                //     nodegroupid: '',
-                //     resourceid: null,
-                //     tileid: null,
-                //     parenttileid: null,
-                //     icon: 'fa-tag',
-                //     nameheading: 'Communication',
-                //     namelabel: '[no label]'
-                // },
+                {
+                    title: 'Related Consultation / Date',
+                    name: 'relatedconsultation',
+                    description: '',
+                    component: 'views/components/workflows/select-resource-step',
+                    componentname: 'select-resource-step',
+                    graphid: '8d41e49e-a250-11e9-9eab-00224800b26d',
+                    nodegroupid: "caf5bff9-a3d7-11e9-a2e7-00224800b26d", // Comm Date
+                    resourceid: null,
+                    tileid: null,
+                    parenttileid: null,
+                    icon: 'fa-tag',
+                    nameheading: 'Communication',
+                    namelabel: '[no label]'
+                },
                 {
                     title: 'Communication Details',
                     description: 'Type of Communication',
