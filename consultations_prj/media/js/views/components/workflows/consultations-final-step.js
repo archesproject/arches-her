46c8350437361a0030a3629aac354d8d3f2871cf
Galen Mancino <whatisgalen@gmail.com>
2019-08-30 15:46:40 -0700

    changes template for consultations-final-step, adds css, re #5151

--- consultations_prj/media/js/views/components/workflows/consultations-final-step.js
+++ consultations_prj/media/js/views/components/workflows/consultations-final-step.js
@@ -4,43 +4,59 @@ define([
     'arches',
     'knockout',
     'knockout-mapping',
-    'views/components/workflows/final-step',
-    'json!plugins/init-workflow.json'
-
-], function(_, $, arches, ko, koMapping, FinalStep, WorkFlowConfig) {
+    'views/components/workflows/final-step'
+], function(_, $, arches, ko, koMapping, FinalStep) {
 
     function viewModel(params) {
-
-        if (!params.resourceid() && params.requirements){
-            params.resourceid(params.requirements.resourceid);
-            params.tileid(params.requirements.tileid);
-        }
-
-        FinalStep.apply(this, [params]);
         var self = this;
-        self.requirements = params.requirements;
+        FinalStep.apply(this, [params]);
+        self.loading(true);
         params.tile = self.tile;
-
-        this.workflows = WorkFlowConfig.workflows.map(function(wf){
-            wf.url = arches.urls.plugin(wf.slug);
-            return wf;
-        }, this);
-        
-
-        params.getStateProperties = function(){
-                return {
-                    resourceid: ko.unwrap(params.resourceid),
-                    tile: !!(params.tile) ? koMapping.toJS(params.tile().data) : undefined,
-                    tileid: !!(params.tile) ? ko.unwrap(params.tile().tileid): undefined
+        this.workflowJSON = ko.observable();
+        this.workflows = ko.observableArray();
+        this.getJSON = function() {
+            $.ajax({
+                type: "GET",
+                url: arches.urls.plugin('init-workflow'),
+                data: {
+                    "json":true
+                },
+                context: self,
+                success: function(data){
+                    // console.log(data);
+                    self.workflowJSON(data);
+                },
+                error: function(response) {
+                    if(response.statusText !== 'abort'){
+                        this.viewModel.alert(new AlertViewModel('ep-alert-red', arches.requestFailed.title, response.responseText));
+                    }
                 }
+            });
+            self.loading(false);
+        };
+        this.getJSON();
+
+        this.workflowJSON.subscribe(function(val){
+            if(val) {
+                self.workflows(val['config']['workflows'].map(function(wf){
+                    wf.url = arches.urls.plugin(wf.slug);
+                    return wf;
+                }, this));
+            }
+        });
+        
+        params.getStateProperties = function(){
+            return {
+                resourceid: ko.unwrap(params.resourceid),
+                tile: !!(params.tile) ? koMapping.toJS(params.tile().data) : undefined,
+                tileid: !!(params.tile) ? ko.unwrap(params.tile().tileid): undefined
             };
+        };
     };
 
-    return ko.components.register('consultations-final-step', {
+    ko.components.register('consultations-final-step', {
         viewModel: viewModel,
-        template: {
-            require: 'text!templates/views/components/workflows/consultations-final-step.htm'
-        }
+        template: { require: 'text!templates/views/components/workflows/consultations-final-step.htm' }
     });
     return viewModel;
 });
