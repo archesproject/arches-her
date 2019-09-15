6b795a9229c04f1f206ba7f0639412ad4089bffe
Cyrus Hiatt <chiatt@fargeo.com>
2019-09-15 07:55:38 -0700

    Redirects user to 'new' workflow page upon quitting a workflow. re #247

--- consultations_prj/media/js/views/components/plugins/communication-workflow.js
+++ consultations_prj/media/js/views/components/plugins/communication-workflow.js
@@ -1,10 +1,11 @@
 define([
     'knockout',
+    'arches',
     'viewmodels/workflow',
     'viewmodels/workflow-step',
     'views/components/workflows/new-tile-step',
     'views/components/workflows/select-resource-step'
-], function(ko, Workflow, Step) {
+], function(ko, arches, Workflow, Step) {
     return ko.components.register('communication-workflow', {
         viewModel: function(params) {
             var self = this;
@@ -75,6 +76,7 @@ define([
             ];
 
             Workflow.apply(this, [params]);
+            this.quitUrl = "/consultations" + arches.urls.plugin('init-workflow');
             self.getJSON('communication-workflow');
 
             self.activeStep.subscribe(this.updateState);
