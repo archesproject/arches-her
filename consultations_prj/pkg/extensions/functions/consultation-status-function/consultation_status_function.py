985d11ef6f9a3ab615ec72970e3f4633eb25070c
Galen Mancino <whatisgalen@gmail.com>
2019-09-23 13:44:03 -0700

    supplies defaults to get_or_create() to fix tile issue, re #286

--- consultations_prj/pkg/extensions/functions/consultation-status-function/consultation_status_function.py
+++ consultations_prj/pkg/extensions/functions/consultation-status-function/consultation_status_function.py
@@ -22,7 +22,6 @@ details = {
 class ConsultationStatusFunction(BaseFunction): 
 
     def save(self, tile, request):
-        
         cons_status_list_nodeid = "8d41e4d3-a250-11e9-8977-00224800b26d"
         cons_status_bool_nodeid = "6a773228-db20-11e9-b6dd-784f435179ea"
 
@@ -34,65 +33,72 @@ class ConsultationStatusFunction(BaseFunction):
             "Dormant",
             "Publication & archiving"
         ]
-        if cons_status_list_nodeid in tile.data.keys():
-            datatype_factory = DataTypeFactory()
-            cons_status_list_node = models.Node.objects.get(nodeid=cons_status_list_nodeid)
-            datatype = datatype_factory.get_instance(cons_status_list_node.datatype)
-            tile_status = datatype.get_display_value(tile, cons_status_list_node)
+        if tile.data is not None:
+            if cons_status_list_nodeid in tile.data.keys():
+                datatype_factory = DataTypeFactory()
+                cons_status_list_node = models.Node.objects.get(nodeid=cons_status_list_nodeid)
+                datatype = datatype_factory.get_instance(cons_status_list_node.datatype)
+                tile_status = datatype.get_display_value(tile, cons_status_list_node)
 
-            status = True if tile_status in active_statuses else False
-            resourceinstance_id = str(tile.resourceinstance.resourceinstanceid)
-            cons_status_tile, created = Tile.objects.get_or_create(
-                resourceinstance_id=resourceinstance_id,
-                nodegroup_id=cons_status_bool_nodeid
-            )
+                status = True if tile_status in active_statuses else False
+                resourceinstance_id = str(tile.resourceinstance.resourceinstanceid)
+                cons_status_tile, created = Tile.objects.get_or_create(
+                    resourceinstance_id=resourceinstance_id,
+                    nodegroup_id=cons_status_bool_nodeid,
+                    defaults = {'data':{"6a773228-db20-11e9-b6dd-784f435179ea":status}}
+                )
 
-            try:
-                cons_status_tile.data = json.dumps({ "6a773228-db20-11e9-b6dd-784f435179ea":status })
-                # cons_status_tile.data = { "6a773228-db20-11e9-b6dd-784f435179ea":status }
-            except Exception as e:
-                print('tile.data assignment error: ',e)
-            try:
-                cons_status_tile.save(request=request)
-            except Exception as e:
-                print('tile.save error: ',e)
-        
-        # else:
-        #     print("not found")
-        #     return
+                if created is False:
+                    try:
+                        # cons_status_tile.data = json.dumps({ "6a773228-db20-11e9-b6dd-784f435179ea":status })
+                        cons_status_tile.data = { "6a773228-db20-11e9-b6dd-784f435179ea":status }
+                    except Exception as e:
+                        print('tile.data assignment error: ',e)
+                    try:
+                        cons_status_tile.save(request=request)
+                    except Exception as e:
+                        print('tile.save error: ',e)
 
         return
 
+    
     def get(self):
         raise NotImplementedError
 
+    
     def delete(self,tile,request):
         cons_status_list_nodeid = "8d41e4d3-a250-11e9-8977-00224800b26d"
         cons_status_bool_nodeid = "6a773228-db20-11e9-b6dd-784f435179ea"
         
-        if cons_status_list_nodeid in tile.data.keys():
-            status = False
+        if tile.data is not None:
+            if cons_status_list_nodeid in tile.data.keys():
+                status = False
 
-            resourceinstance_id = str(tile.resourceinstance.resourceinstanceid)
-            cons_status_tile, created = Tile.objects.get_or_create(
-                resourceinstance_id=resourceinstance_id,
-                nodegroup_id=cons_status_bool_nodeid
-            )
+                resourceinstance_id = str(tile.resourceinstance.resourceinstanceid)
+                cons_status_tile, created = Tile.objects.get_or_create(
+                    resourceinstance_id=resourceinstance_id,
+                    nodegroup_id=cons_status_bool_nodeid,
+                    defaults = {'data':{"6a773228-db20-11e9-b6dd-784f435179ea":status}}
+                )
 
-            try:
-                cons_status_tile.data = json.dumps({ "6a773228-db20-11e9-b6dd-784f435179ea":status })
-                # cons_status_tile.data = { "6a773228-db20-11e9-b6dd-784f435179ea":status }
-            except Exception as e:
-                print('tile.data assignment error: ',e)
-            try:
-                cons_status_tile.save(request=request)
-            except Exception as e:
-                print('tile.save error: ',e)
+                if created is False:
+                    try:
+                        # cons_status_tile.data = json.dumps({ "6a773228-db20-11e9-b6dd-784f435179ea":status })
+                        cons_status_tile.data = { "6a773228-db20-11e9-b6dd-784f435179ea":status }
+                    except Exception as e:
+                        print('tile.data assignment error: ',e)
+                    try:
+                        cons_status_tile.save(request=request)
+                    except Exception as e:
+                        print('tile.save error: ',e)
+
         return
 
+    
     def on_import(self,tile):
         raise NotImplementedError
 
+    
     def after_function_save(self,tile,request):
         raise NotImplementedError
     
