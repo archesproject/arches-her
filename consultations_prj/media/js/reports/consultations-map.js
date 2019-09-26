b7be05badd59908ada71c9e029add654800502d5
Rob Gaston <rgaston@fargeo.com>
2019-09-26 16:05:56 -0700

    implements consultations map header, re: #242

--- consultations_prj/media/js/reports/consultations-map.js
+++ consultations_prj/media/js/reports/consultations-map.js
@@ -1,10 +1,54 @@
 define([
-    'knockout'
-], function(ko) {
+    'knockout',
+    'knockout-mapping',
+    'underscore',
+    'geojson-extent',
+    'views/components/map',
+    'views/components/cards/select-feature-layers',
+], function(ko, koMapping, _, geojsonExtent, MapComponentViewModel, selectFeatureLayersFactory) {
     ko.components.register('consultations-map', {
-        viewModel: function() {
-            this.text = 'Consultations map....';
+        viewModel: function(params) {
+            var self = this;
+            var featureCollection = ko.computed(function() {
+                var features = [];
+                ko.unwrap(params.tiles).forEach(function(tile) {
+                    _.each(tile.data, function(val) {
+                        if ('features' in val) {
+                            features = features.concat(koMapping.toJS(val.features));
+                        }
+                    }, this);
+                }, this);
+                return {
+                    type: 'FeatureCollection',
+                    features: features
+                };
+            });
+            
+            if (featureCollection().features.length > 0) {
+                params.bounds = geojsonExtent(featureCollection());
+                params.fitBoundsOptions = { padding: 40 };
+            }
+            
+            params.activeTab = false;
+            params.sources = Object.assign({
+                "consultations-map-data": {
+                    "type": "geojson",
+                    "data": featureCollection()
+                }
+            }, params.sources);
+            params.layers = selectFeatureLayersFactory(
+                '',
+                'consultations-map-data',
+                undefined,
+                [],
+                true
+            );
+            MapComponentViewModel.apply(this, [params]);
+            
+            featureCollection.subscribe(function(featureCollection) {
+                self.map().getSource('consultations-map').setData(featureCollection);
+            });
         },
-        template: '<div data-bind="text: text"></div>'
+        template: { require: 'text!templates/views/components/map.htm' }
     });
 });
