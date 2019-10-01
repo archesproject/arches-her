9cca6f641307dcd36d7a6bfd233cc00d74a5bf4a
Galen Mancino <whatisgalen@gmail.com>
2019-09-30 17:09:13 -0700

    re-arranges the report template to move table cards into own loops, creates new viewmodel to tabulate data, re #241

---
+++ consultations_prj/media/js/views/components/cards/default.js
@@ -0,0 +1,46 @@
+define([
+    'knockout',
+    'viewmodels/card-component'
+], function(ko, CardViewModel) {
+
+    function viewmodel(params) {
+        CardViewModel.apply(this, [params]);
+        var self = this;
+
+        this.getTableData = function(widgets, tiles) {
+            var tileObjArr = [[]]
+            var tilesArr = ko.unwrap(tiles);
+            var widgets = ko.unwrap(widgets);
+            var widget = null, tile = null;
+            
+            for(var i = 0, j = 0; i < tilesArr.length; j++) {
+                if (!tileObjArr[i]) { tileObjArr[i] = []; }
+                widget = widgets[j], tile = tilesArr[i];
+                if (tile) {
+                    tileObjArr[i][j] = {
+                        name: widget.widgetLookup[widget.widget_id()].name,
+                        node: widget.node,
+                        config: widget.config,
+                        label: widget.label,
+                        value: tile.data[widget.node_id()],
+                        tile: tile
+                    };
+                }
+                if(j >= 1) { 
+                    j = -1;
+                    i++;
+                }
+            }
+            // console.log(ko.unwrap(tileObjArr));
+            return tileObjArr;
+        };
+
+    }
+
+    return ko.components.register('default-card', {
+        viewModel: viewmodel,
+        template: {
+            require: 'text!templates/views/components/cards/default.htm'
+        }
+    });
+});
