9778ce4910ff56496e81aef5e99baaa4480e52d8
Galen Mancino <whatisgalen@gmail.com>
2019-10-28 14:10:59 -0700

    re-inserts basemgr view, re #387

--- consultations_prj/media/js/about.js
+++ consultations_prj/media/js/about.js
@@ -1,21 +1,20 @@
 define([
     'knockout',
-    // 'views/base-manager'
-], function(ko) {
+    'views/base-manager'
+], function(ko, BaseManagerView) {
     function aboutViewModel(params) {
         /** L#74 of arches base.htm requires this file to be in media/js/ **/
-        // BaseManagerView.apply(this,[params]);
+        BaseManagerView.apply(this,[params]);
         var self = this;
         self.loading(false);
         self.visible(true);
         console.log(ko.unwrap(self));
 
     }
-    return ko.components.register('about', {
+    ko.components.register('about', {
         viewModel: aboutViewModel,
         template: {
             require: 'text!templates/about.htm'
         }
     });
-    return aboutViewModel;
 });
