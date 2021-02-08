c7ca76bc3b8a16c8830a9fcee87bf06d726ef3b3
phudson-he <paul.hudson@historicengland.org.uk>
2021-02-08 13:03:45 +0000

    Update bng files to 5.1

--- arches_her/datatypes/bngcentrepoint.py
+++ arches_her/datatypes/bngcentrepoint.py
@@ -5,57 +5,110 @@ from arches.app.models.system_settings import settings
 bngpoint = Widget.objects.get(name="bngpoint")
 
 details = {
-    'datatype': 'bngcentrepoint',
-    'iconclass': 'fa fa-location-arrow',
-    'modulename': 'bngcentrepoint.py',
-    'classname': 'BNGCentreDataType',
-    'defaultwidget': bngpoint,
-    'defaultconfig': None,
-    'configcomponent': None,
-    'configname': None,
-    'isgeometric': False,
-    'issearchable': False
+    "datatype": "bngcentrepoint",
+    "iconclass": "fa fa-location-arrow",
+    "modulename": "bngcentrepoint.py",
+    "classname": "BNGCentreDataType",
+    "defaultwidget": bngpoint,
+    "defaultconfig": None,
+    "configcomponent": None,
+    "configname": None,
+    "isgeometric": False,
+    "issearchable": False,
 }
 
+
 class BNGCentreDataType(BaseDataType):
-
-
-    def validate(self, value, row_number=None, source=None):
+    def validate(self, value, row_number=None, source=None, node=None, nodeid=None):
 
         errors = []
-        gridSquareArray = ["HP","HT","HU","HW","HX","HY","HZ","NA","NB","NC","ND","NF","NG","NH","NJ","NK","NL", "NM","NN","NO","NR","NS","NT","NU","NW","NX","NY","NZ","SC","SD","SE","SH","SJ","SK","SM","SN","SO","SP","SR","SS","ST","SU","SV","SW","SX","SY","SZ","TA","TF","TG","TL","TM","TQ","TR","TV"]
+        gridSquareArray = [
+            "NA",
+            "NB",
+            "NC",
+            "ND",
+            "NE",
+            "NF",
+            "NG",
+            "NH",
+            "NJ",
+            "NK",
+            "NL",
+            "NM",
+            "NN",
+            "NO",
+            "NP",
+            "NR",
+            "NS",
+            "NT",
+            "NU",
+            "NV",
+            "NW",
+            "NX",
+            "NY",
+            "NZ",
+            "OA",
+            "OB",
+            "OF",
+            "OG",
+            "OL",
+            "OM",
+            "OQ",
+            "OR",
+            "OV",
+            "OW",
+            "SA",
+            "SB",
+            "SC",
+            "SD",
+            "SE",
+            "SF",
+            "SG",
+            "SH",
+            "SJ",
+            "SK",
+            "SL",
+            "SM",
+            "SN",
+            "SO",
+            "SP",
+            "SQ",
+            "SR",
+            "SS",
+            "ST",
+            "SU",
+            "SV",
+            "SW",
+            "SX",
+            "SY",
+            "SZ",
+            "TA",
+            "TB",
+            "TF",
+            "TG",
+            "TL",
+            "TM",
+            "TQ",
+            "TR",
+            "TV",
+            "TW",
+        ]
         try:
             # CS - Validation for datatype.  Replicates functionality in widget which will be removed once datatype validation is fixed.
-            firstTwoCharacters =  value[0:2]
+            firstTwoCharacters = value[0:2]
             numberElement = value[2:]
 
             firstTwoCharacters in gridSquareArray
-            isinstance(int(numberElement),int)
+            isinstance(int(numberElement), int)
             len(value) == 12
-        except KeyError:
-            errors.append({
-                'type':'ERROR',
-                'message': 'Issue with input data'
-            })
+        except Exception:
+            errors.append({"type": "ERROR", "message": "Issue with input data"})
 
-        return errors 
+        return errors
 
+    def append_to_document(self, document, nodevalue, nodeid, tile, provisional=False):
 
-    def default_es_mapping(self):
-        bng_mapping = {"type": "text", "fields": {"keyword": {"ignore_above": 256, "type": "keyword"}}}
-        return bng_mapping
-
-
-    def append_to_document(self, document, nodevalue, nodeid, tile):
-
-        document['strings'].append({
-            'string': nodevalue,
-            'nodegroup_id': tile.nodegroup_id
-            })
-
-
+        document["strings"].append({"string": nodevalue, "nodegroup_id": tile.nodegroup_id})
 
     def get_search_terms(self, nodevalue, nodeid=None):
-        return [
-            nodevalue
-        ]
+        return [nodevalue]
