736e496c1d58977225690304ca1d9eabc45b693b
njkim <nkim@fargeo.com>
2021-04-06 17:46:38 -0700

    Update logic if more than one communication, #587

--- arches_her/media/js/views/components/workflows/communication-workflow/communication-final-step.js
+++ arches_her/media/js/views/components/workflows/communication-workflow/communication-final-step.js
@@ -9,16 +9,27 @@ define([
         this.resourceData = ko.observable();
 
         this.resourceData.subscribe(function(val){
+            var currentCommunication;
+            if (Array.isArray(val.resource.Communications)){
+                /*val.resource.Communications.forEach(function(comm) {
+                    if (comm['Communication Type']['@tile_id'] === params.workflow.getStepData("related-consultation")['tileid']){
+                        currentCommunication = comm;
+                    }
+                });*/
+                currentCommunication = val.resource.Communications[val.resource.Communications.length-1];
+            } else {
+                currentCommunication = val.resource.Communications;
+            }
             this.reportVals = {
                 consultationName: {'name': 'Consultation', 'value': val.resource['Consultation Names']['Consultation Name']['@value'] || 'none'},
-                subject: {'name': 'Subject', 'value': val.resource['Communications']['Subjects']['Subject']['@value'] || 'none'},
-                type: {'name': 'Type', 'value': val.resource['Communications']['Communication Type']['@value'] || 'none'},
-                date: {'name': 'Date', 'value': val.resource['Communications']['Dates']['Date']['@value'] || 'none'},
-                participants: {'name': 'Participants', 'value': val.resource['Communications']['Attendees']['@value'] || 'none'},
-                relatedCondition: {'name': 'Related Condition', 'value': val.resource['Communications']['Related Condition']['@value'] || 'none'},
-                notes: {'name': 'Notes', 'value': val.resource['Communications']['Communication Notes']['Communication Description']['@value'] || 'none'},
-                followOnActions: {'name': 'Follow-on Actions', 'value': val.resource['Communications']['Follow on Actions']['Follow-On Actions']['@value'] || 'none'},
-                uploadedFiles: {'name': 'Uploaded Files', 'value': val.resource['Communications']['Digital File(s)']['@value'] || 'none'},
+                subject: {'name': 'Subject', 'value': currentCommunication['Subjects']['Subject']['@value'] || 'none'},
+                type: {'name': 'Type', 'value': currentCommunication['Communication Type']['@value'] || 'none'},
+                date: {'name': 'Date', 'value': currentCommunication['Dates']['Date']['@value'] || 'none'},
+                participants: {'name': 'Participants', 'value': currentCommunication['Attendees']['@value'] || 'none'},
+                relatedCondition: {'name': 'Related Condition', 'value': currentCommunication['Related Condition']['@value'] || 'none'},
+                notes: {'name': 'Notes', 'value': currentCommunication['Communication Notes']['Communication Description']['@value'] || 'none'},
+                followOnActions: {'name': 'Follow-on Actions', 'value': currentCommunication['Follow on Actions']['Follow-On Actions']['@value'] || 'none'},
+                uploadedFiles: {'name': 'Uploaded Files', 'value': currentCommunication['Digital File(s)']['@value'] || 'none'},
             }
             this.loading(false);
         }, this);
