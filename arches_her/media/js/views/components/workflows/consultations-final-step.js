07e899eeed9e7955fdd093515c29ce66090deb01
Cyrus Hiatt <chiatt@fargeo.com>
2021-04-12 18:08:53 -0700

    Initial work on consultation step template, re #598

--- arches_her/media/js/views/components/workflows/consultations-final-step.js
+++ arches_her/media/js/views/components/workflows/consultations-final-step.js
@@ -1,22 +1,31 @@
 define([
     'knockout',
     'views/components/workflows/summary-step',
-    'geojson-extent',
-    'views/components/map',
-    'views/components/cards/select-feature-layers',
-    'viewmodels/alert'
-], function(ko, SummaryStep, geojsonExtent, MapComponentViewModel, selectFeatureLayersFactory, AlertViewModel) {
+], function(ko, SummaryStep) {
 
     function viewModel(params) {
         SummaryStep.apply(this, [params]);
 
         this.resourceData.subscribe(function(val){
             this.reportVals = {
-                // buildingName: {'name': 'Building Name', 'value': val.resource['Addresses']['Building Name']['Building Name Value']['@value'] || 'none'},
+                featureShape: {'name': 'Feature Shape', 'value': val.resource['Consultation Area']['Geometry']['Feature Shape']['@value'] || 'none'},
+                logDate: {'name': 'Log Date', 'value': val.resource['Consultation Dates']['Log Date']['@value'] || 'none'},
+                targetDate: {'name': 'Target Date', 'value': val.resource['Consultation Dates']['Target Date']['Target Date Start']['@value'] || 'none'},
+                consultationType: {'name': 'Consultation Type', 'value': val.resource['Consultation Type']['@value'] || 'none'},
+                applicationType: {'name': 'Application Type', 'value': val.resource['Application Type']['@value'] || 'none'},
+                developmentType: {'name': 'Development Type', 'value': val.resource['Development Type']['@value'] || 'none'},
+                references: val.resource['References'].map(function(ref){
+                    return {
+                        referenceName: {'name': 'Reference', 'value': ref['Agency Identifier']['Reference']['@value'] || 'none'},
+                        referenceType: {'name': 'Reference Type', 'value': ref['Agency Identifier']['Reference Type']['@value'] || 'none'},
+                        agency: {'name': 'Agency', 'value': ref['Agency']['@value'] || 'none'},
+                    };
+                }) || 'none',
+                proposalDescription: {'name': 'Proposal Description', 'value': val.resource['Proposal']['Proposal Text']['@value'] || 'none'},
             };
-            // var geojsonStr = val.resource['Geometry']['Geospatial Coordinates']['@value'].replaceAll("'", '"');
-            // var geojson = JSON.parse(geojsonStr);
-            // this.prepareMap(geojson, 'app-area-map-data');
+            var geojsonStr = val.resource['Consultation Area']['Geometry']['Geospatial Coordinates']['@value'].replaceAll("'", '"');
+            var geojson = JSON.parse(geojsonStr);
+            this.prepareMap(geojson, 'consultation-map-data');
             this.loading(false);
         }, this);
     }
