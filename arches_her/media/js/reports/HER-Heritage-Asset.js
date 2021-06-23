0a3cc3e0618d27cf6a535e39b4cdc297f8e6e0c1
Dennis Wuthrich <dwuthrich@fargeo.com>
2021-06-23 11:59:10 -0700

    Add custom breakpoints for names table

--- arches_her/media/js/reports/HER-Heritage-Asset.js
+++ arches_her/media/js/reports/HER-Heritage-Asset.js
@@ -26,7 +26,19 @@ define([
                 ReportViewModel.apply(this, [params]);
                 this.activeSection = ko.observable('name')
                 this.nameTableConfig = {
-                    "responsive": true,
+                    "responsive": {
+                        breakpoints: [
+                          {name: 'bigdesktop', width: Infinity},
+                          {name: 'meddesktop', width: 1480},
+                          {name: 'smalldesktop', width: 1280},
+                          {name: 'medium', width: 1188},
+                          {name: 'tabletl', width: 1024},
+                          {name: 'btwtabllandp', width: 848},
+                          {name: 'tabletp', width: 768},
+                          {name: 'mobilel', width: 480},
+                          {name: 'mobilep', width: 320}
+                        ]
+                      },
                     "paging": false,
                     "searching": false,
                     "scrollCollapse": true,
