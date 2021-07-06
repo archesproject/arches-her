5e9057cb9f1adea38d48b7b3d3d78d880d36908f
Dennis Wuthrich <dwuthrich@fargeo.com>
2021-07-06 11:35:39 -0700

    Add designation/protection card

--- arches_her/media/js/reports/HER-Heritage-Asset.js
+++ arches_her/media/js/reports/HER-Heritage-Asset.js
@@ -160,6 +160,62 @@ define([
                     ]
                 };
 
+                //Designation table configuration
+                this.designationTableConfig = {
+                    "responsive": {
+                        breakpoints: [
+                          {name: 'bigdesktop', width: Infinity},
+                          {name: 'meddesktop', width: 1480},
+                          {name: 'smalldesktop', width: 1280},
+                          {name: 'medium', width: 1188},
+                          {name: 'tabletl', width: 1024},
+                          {name: 'btwtabllandp', width: 848},
+                          {name: 'tabletp', width: 768},
+                          {name: 'mobilel', width: 480},
+                          {name: 'mobilep', width: 320}
+                        ]
+                      },
+                    "paging": false,
+                    "searching": false,
+                    "scrollCollapse": true,
+                    "info": false,
+                    "columnDefs": [ {
+                        "orderable": false,
+                        "targets":   -1
+                    } ],
+                    "columns": [
+                        null,
+                        null,
+                        null,
+                        null,
+                        null,
+                        null,
+                        null,
+                        null,
+                        null,
+                        null,
+                        null
+                    ]
+                };
+
+                //Files related to designations Table
+                this.designationFilesTableConfig = {
+                    "responsive": true,
+                    "paging": false,
+                    "searching": false,
+                    "scrollCollapse": true,
+                    "info": false,
+                    "columnDefs": [ {
+                        "orderable": false,
+                        "targets":   -1
+                    } ],
+                    "columns": [
+                        null,
+                        null,
+                        null
+                    ]
+                };
+
                 //Construction Phases table configuration
                 this.conPhasesTableConfig = {
                     "responsive": {
