81b8baed948712763f577acf3f705962e85ad880
Cyrus Hiatt <chiatt@fargeo.com>
2021-11-16 15:05:47 -0800

    Update workflow file to work with arches 6.0

--- arches_her/media/js/views/components/plugins/consultation-workflow.js
+++ arches_her/media/js/views/components/plugins/consultation-workflow.js
@@ -64,145 +64,130 @@ define([
                     ],
                     required: false
                 },
-                // {
-                //     title: 'Consultation Details/Type',
-                //     name: 'set-cons-details',
-                //     icon: 'fa-list-alt',
-                //     description: 'Consultation Details',
-                //     informationboxdata: {
-                //         heading: 'Consultation Details',
-                //         text: 'Select the type of consultation',
-                //     },
-                //     component: 'views/components/workflows/component-based-step',
-                //     componentname: 'component-based-step',
-                //     required: true,
-                //     layoutSections: [
-                //         {
-                //             componentConfigs: [
-                //                 { 
-                //                     componentName: 'grouping-card-component',
-                //                     uniqueInstanceName: 'app-cons-details', /* unique to step */
-                //                     tilesManaged: 'one',
-                //                     parameters: {
-                //                         graphid: '8d41e49e-a250-11e9-9eab-00224800b26d',
-                //                         nodegroupid: '771bb1e2-8895-11ea-8446-f875a44e0e11', //consultation type node
-                //                     },
-                //                 },
-                //             ], 
-                //         },
-                //     ],
-                // },
-                // {
-                //     title: 'Reference Numbers',
-                //     name: 'set-ref-numbers',
-                //     description: 'Application Reference Numbers',
-                //     informationboxdata: {
-                //         heading: 'Application Reference Numbers',
-                //         text: 'Save one or more reference numbers before moving to the next step',
-                //     },
-                //     icon: 'fa-hashtag',
-                //     component: 'views/components/workflows/component-based-step',
-                //     componentname: 'component-based-step',
-                //     required: true,
-                //     layoutSections: [
-                //         {
-                //             componentConfigs: [
-                //                 { 
-                //                     componentName: 'default-card',
-                //                     uniqueInstanceName: 'app-ref-numbers', /* unique to step */
-                //                     tilesManaged: 'many',
-                //                     parameters: {
-                //                         graphid: '8d41e49e-a250-11e9-9eab-00224800b26d',
-                //                         nodegroupid: '8d41e4a2-a250-11e9-82f1-00224800b26d',
-                //                     },
-                //                 },
-                //             ], 
-                //         },
-                //     ],
-                // },
-                // {
-                //     title: 'Application Proposal',
-                //     name: 'application-proposal',
-                //     icon: 'fa-clipboard',
-                //     description: 'Summary of the Application that will be reviewed under this Consultation',
-                //     informationboxdata: {
-                //         heading: 'Application Proposal',
-                //         text: 'Summary of the application that will be reviewed under this consultation',
-                //     },
-                //     component: 'views/components/workflows/component-based-step',
-                //     componentname: 'component-based-step',
-                //     required: true,
-                //     layoutSections: [
-                //         {
-                //             componentConfigs: [
-                //                 { 
-                //                     componentName: 'default-card',
-                //                     uniqueInstanceName: 'app-proposal', /* unique to step */
-                //                     tilesManaged: 'one',
-                //                     parameters: {
-                //                         graphid: '8d41e49e-a250-11e9-9eab-00224800b26d',
-                //                         nodegroupid: '1b0e15e9-8864-11ea-b5f3-f875a44e0e11',
-                //                     },
-                //                 },
-                //             ], 
-                //         },
-                //     ],
-                // },
-                // {
-                //     title: 'Contacts',
-                //     icon: 'fa-users',
-                //     name: 'consultation-contacts',
-                //     description: 'Identify the key people/organizations associated with this consultation',
-                //     informationboxdata: {
-                //         heading: 'Contacts',
-                //         text: 'Identify the key people/organizations associated with this consultation',
-                //     },
-                //     required: false,
-                //     component: 'views/components/workflows/component-based-step',
-                //     componentname: 'component-based-step',
-                //     layoutSections: [
-                //         {
-                //             componentConfigs: [
-                //                 { 
-                //                     componentName: 'default-card',
-                //                     uniqueInstanceName: 'app-contacts', /* unique to step */
-                //                     tilesManaged: 'one',
-                //                     parameters: {
-                //                         graphid: '8d41e49e-a250-11e9-9eab-00224800b26d',
-                //                         nodegroupid: '4ea4a189-184f-11eb-b45e-f875a44e0e11',
-                //                     },
-                //                 },
-                //             ], 
-                //         },
-                //     ],
-                // },
-                // {
-                //     title: 'Add Consulation Complete',
-                //     name: 'consultation-complete',
-                //     description: 'Choose an option below',
-                //     component: 'views/components/workflows/component-based-step',
-                //     componentname: 'component-based-step',
-                //     layoutSections: [
-                //         {
-                //             componentConfigs: [
-                //                 { 
-                //                     componentName: 'consultations-final-step',
-                //                     uniqueInstanceName: 'consultation-final', /* unique to step */
-                //                     tilesManaged: 'none',
-                //                     parameters: {},
-                //                 },
-                //             ], 
-                //         },
-                //     ],
-                //     graphid: '42ce82f6-83bf-11ea-b1e8-f875a44e0e11',
-                //     resourceid: null,
-                //     tileid: null,
-                //     parenttileid: null,
-                //     informationboxdata: {
-                //         heading: 'Workflow Complete: Review your work',
-                //         text: 'Please review the summary information. You can go back to a previous step to make changes or "Quit Workflow" to discard your changes and start over',
-                //     }
-                // },
+                {
+                    title: 'Consultation Details/Type',
+                    name: 'set-cons-details',
+                    informationboxdata: {
+                        heading: 'Consultation Details',
+                        text: 'Select the type of consultation',
+                    },
+                    required: true,
+                    layoutSections: [
+                        {
+                            componentConfigs: [
+                                { 
+                                    componentName: 'grouping-card-component',
+                                    uniqueInstanceName: 'app-cons-details', /* unique to step */
+                                    tilesManaged: 'one',
+                                    parameters: {
+                                        graphid: '8d41e49e-a250-11e9-9eab-00224800b26d',
+                                        nodegroupid: '771bb1e2-8895-11ea-8446-f875a44e0e11',  //consultation type node
+                                        resourceid: "['consultation-location']['consultation-map-step'][0]['resourceid']",
+                                    },
+                                },
+                            ], 
+                        },
+                    ],
+                },
+                {
+                    title: 'Reference Numbers',
+                    name: 'set-ref-numbers',
+                    informationboxdata: {
+                        heading: 'Application Reference Numbers',
+                        text: 'Save one or more reference numbers before moving to the next step',
+                    },
+                    required: true,
+                    layoutSections: [
+                        {
+                            componentConfigs: [
+                                { 
+                                    componentName: 'default-card',
+                                    uniqueInstanceName: 'app-ref-numbers', /* unique to step */
+                                    tilesManaged: 'many',
+                                    parameters: {
+                                        title: 'cats',
+                                        graphid: '8d41e49e-a250-11e9-9eab-00224800b26d',
+                                        nodegroupid: '8d41e4a2-a250-11e9-82f1-00224800b26d',
+                                        resourceid: "['consultation-location']['consultation-map-step'][0]['resourceid']",
+                                    },
+                                },
+                            ], 
+                        },
+                    ],
+                },
+                {
+                    title: 'Application Proposal',
+                    name: 'application-proposal',
+                    informationboxdata: {
+                        heading: 'Application Proposal',
+                        text: 'Summary of the application that will be reviewed under this consultation',
+                    },
+                    required: true,
+                    layoutSections: [
+                        {
+                            componentConfigs: [
+                                { 
+                                    componentName: 'default-card',
+                                    uniqueInstanceName: 'app-proposal', /* unique to step */
+                                    tilesManaged: 'one',
+                                    parameters: {
+                                        graphid: '8d41e49e-a250-11e9-9eab-00224800b26d',
+                                        nodegroupid: '1b0e15e9-8864-11ea-b5f3-f875a44e0e11',
+                                        resourceid: "['consultation-location']['consultation-map-step'][0]['resourceid']",
+                                    },
+                                },
+                            ], 
+                        },
+                    ],
+                },
+                {
+                    title: 'Contacts',
+                    name: 'consultation-contacts',
+                    informationboxdata: {
+                        heading: 'Contacts',
+                        text: 'Identify the key people/organizations associated with this consultation',
+                    },
+                    required: false,
+                    layoutSections: [
+                        {
+                            componentConfigs: [
+                                { 
+                                    componentName: 'default-card',
+                                    uniqueInstanceName: 'app-contacts', /* unique to step */
+                                    tilesManaged: 'one',
+                                    parameters: {
+                                        graphid: '8d41e49e-a250-11e9-9eab-00224800b26d',
+                                        nodegroupid: '4ea4a189-184f-11eb-b45e-f875a44e0e11',
+                                        resourceid: "['consultation-location']['consultation-map-step'][0]['resourceid']",
+                                    },
+                                },
+                            ], 
+                        },
+                    ],
+                },
+                {
+                    title: 'Add Consulation Complete',
+                    name: 'consultation-complete',
+                    description: 'Choose an option below',
+                    layoutSections: [
+                        {
+                            componentConfigs: [
+                                { 
+                                    componentName: 'consultations-final-step',
+                                    uniqueInstanceName: 'consultation-final', /* unique to step */
+                                    tilesManaged: 'none',
+                                    parameters: {
+                                        resourceid: "['consultation-location']['consultation-map-step'][0]['resourceid']",
+                                    },
+                                },
+                            ], 
+                        },
+                    ],
+                    informationboxdata: {
+                        heading: 'Workflow Complete: Review your work',
+                        text: 'Please review the summary information. You can go back to a previous step to make changes or "Quit Workflow" to discard your changes and start over',
+                    }
+                },
             ];
 
             Workflow.apply(this, [params]);
