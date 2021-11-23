b9d110c724433aa8326a0c6763a0298d87d66908
njkim <nkim@fargeo.com>
2021-11-23 11:57:03 -0800

    Update config, #709

--- arches_her/media/js/views/components/plugins/site-visit.js
+++ arches_her/media/js/views/components/plugins/site-visit.js
@@ -3,61 +3,54 @@ define([
     'arches',
     'viewmodels/workflow',
     'viewmodels/workflow-step',
-    'views/components/workflows/new-tile-step',
     'views/components/workflows/select-resource-step',
+    'views/components/workflows/photo-gallery-step',
     'views/components/workflows/site-visit/site-visit-final-step'
-], function(ko, arches, Workflow, Step) {
+], function(ko, arches, Workflow) {
     return ko.components.register('site-visit', {
         viewModel: function(params) {
-            /*if (!params.resourceid) {
-                params.resourceid = ko.observable();
-            }*/
-            var self = this;
-            this.resourceId = ko.observable()
+            this.componentName = 'site-visit';
 
-            params.steps = [
+            this.stepConfig = [
                 {
                     title: 'Site Visit Details',
                     name: 'site-visit-details',
-                    description: 'Site Visit Details - Related Consultation',
-                    component: 'views/components/workflows/select-resource-step',
-                    componentname: 'select-resource-step',
-                    graphid: '8d41e49e-a250-11e9-9eab-00224800b26d',
-                    nodegroupid: '066cb8f0-a251-11e9-85d5-00224800b26d', //Visit Date & desc
-                    resourceid: null,
-                    tileid: null,
-                    parenttileid: null,
                     required: true,
-                    icon: 'fa-tag',
-                    nameheading: 'New Site Visit',
-                    namelabel: '[no label]',
-                    shouldtrackresource: true,
                     informationboxdata: {
                         heading: 'Site Visit Details',
                         text: 'Select a consultation and enter datails for the site visit',
-                    },
-                    wastebin: {tile: null, description: 'a site visit instance'}
-                },
-                {
-                    title: 'Site Visit Attendees',
-                    name: 'site-visit-attendees',
-                    description: 'Site Visit Attendees',
-                    component: 'views/components/workflows/component-based-step',
-                    componentname: 'component-based-step',
-                    externalstepdata: { 
-                        sitevisitedetailsstep: 'site-visit-details',
                     },
                     layoutSections: [
                         {
                             componentConfigs: [
                                 { 
+                                    componentName: 'select-resource-step',
+                                    uniqueInstanceName: 'site-visit-details', /* unique to step */
+                                    tilesManaged: 'one',
+                                    parameters: {
+                                        graphid: '8d41e49e-a250-11e9-9eab-00224800b26d', //consultation graph
+                                        nodegroupid: '066cb8f0-a251-11e9-85d5-00224800b26d', //Visit Date & desc
+                                    },
+                                },
+                            ],
+                        },
+                    ],
+                },
+                {
+                    title: 'Site Visit Attendees',
+                    name: 'site-visit-attendees',
+                    layoutSections: [
+                        {
+                            componentConfigs: [
+                                { 
                                     componentName: 'default-card',
-                                    uniqueInstanceName: 'foo', /* unique to step */
+                                    uniqueInstanceName: 'site-visit-attendees', /* unique to step */
                                     tilesManaged: 'many',
                                     parameters: {
                                         graphid: '8d41e49e-a250-11e9-9eab-00224800b26d',
                                         nodegroupid: 'ab622f1f-a251-11e9-bda5-00224800b26d',
-                                        parenttilesourcestep: 'sitevisitedetailsstep'
+                                        resourceid: "['site-visit-details']['site-visit-details']['resourceInstanceId']",
+                                        parenttileid: "['site-visit-details']['site-visit-details']['tileId']"
                                     },
                                 },
                             ], 
@@ -71,54 +64,44 @@ define([
                 {
                     title: 'Site Visit Observations',
                     name: 'site-visit-observations',
-                    description: '',
-                    component: 'views/components/workflows/component-based-step',
-                    componentname: 'component-based-step',
-                    externalstepdata: { 
-                        sitevisitedetailsstep: 'site-visit-details',
+                    required: false,
+                    informationboxdata: {
+                        heading: 'Site Visit Observations',
+                        text: 'Add the observations during the site visit',
                     },
                     layoutSections: [
                         {
                             componentConfigs: [
                                 { 
                                     componentName: 'default-card',
-                                    uniqueInstanceName: 'foo', /* unique to step */
+                                    uniqueInstanceName: 'site-visit-observations', /* unique to step */
                                     tilesManaged: 'one',
                                     parameters: {
                                         graphid: '8d41e49e-a250-11e9-9eab-00224800b26d',
                                         nodegroupid: 'a2b47f25-938d-11ea-82cb-f875a44e0e11',
-                                        parenttilesourcestep: 'sitevisitedetailsstep'
+                                        resourceid: "['site-visit-details']['site-visit-details']['resourceInstanceId']",
+                                        parenttileid: "['site-visit-details']['site-visit-details']['tileId']"
                                     },
                                 },
                             ], 
                         },
                     ],
-                    required: false,
-                    informationboxdata: {
-                        heading: 'Site Visit Observations',
-                        text: 'Add the observations during the site visit',
-                    }
                 },
                 {
                     title: 'Recommendations',
                     name: 'site-recommendations',
-                    description: '',
-                    component: 'views/components/workflows/component-based-step',
-                    componentname: 'component-based-step',
-                    externalstepdata: { 
-                        sitevisitedetailsstep: 'site-visit-details',
-                    },
                     layoutSections: [
                         {
                             componentConfigs: [
                                 { 
                                     componentName: 'default-card',
-                                    uniqueInstanceName: 'foo', /* unique to step */
+                                    uniqueInstanceName: 'site-recommendations', /* unique to step */
                                     tilesManaged: 'one',
                                     parameters: {
                                         graphid: '8d41e49e-a250-11e9-9eab-00224800b26d',
                                         nodegroupid: 'f5ecfbca-938b-11ea-865d-f875a44e0e11',
-                                        parenttilesourcestep: 'sitevisitedetailsstep'
+                                        resourceid: "['site-visit-details']['site-visit-details']['resourceInstanceId']",
+                                        parenttileid: "['site-visit-details']['site-visit-details']['tileId']"
                                     },
                                 },
                             ], 
@@ -133,23 +116,24 @@ define([
                 {
                     title: 'Site Photos (Upload)',
                     name: 'site-photos-upload',
-                    description: '',
-                    component: 'views/components/workflows/photo-gallery-step',
-                    componentname: 'photo-gallery-step',
-                    graphid: '8d41e49e-a250-11e9-9eab-00224800b26d',
-                    nodegroupid: '3a79a0dd-4535-11eb-b88f-f875a44e0e11',
-                    resourceid: null,
-                    tileid: null,
-                    parenttileid: null,
                     required: false,
-                    externalstepdata: { 
-                        sitevisitedetailsstep: 'site-visit-details',
-                    },
-                    icon: 'fa-camera',
-                    nameheading: 'New Site Visit',
-                    namelabel: '[no label]',
-                    createTile: false,
-                    autoAdvance: false,
+                    layoutSections: [
+                        {
+                            componentConfigs: [
+                                { 
+                                    componentName: 'photo-gallery-step',
+                                    uniqueInstanceName: 'site-photos-upload', /* unique to step */
+                                    tilesManaged: 'one',
+                                    parameters: {
+                                        graphid: '8d41e49e-a250-11e9-9eab-00224800b26d',
+                                        nodegroupid: '3a79a0dd-4535-11eb-b88f-f875a44e0e11',
+                                        resourceid: "['site-visit-details']['site-visit-details']['resourceInstanceId']",
+                                        parenttileid: "['site-visit-details']['site-visit-details']['tileId']"
+                                    },
+                                },
+                            ], 
+                        },
+                    ],
                     informationboxdata: {
                         heading: 'Upload Site Photos',
                         text: 'Upload photographs from the visit by dragging and dropping or clicking the button',
@@ -158,11 +142,9 @@ define([
                 {
                     title: 'Site Visit Workflow Complete',
                     name: 'site-visit-complete',
-                    description: '',
-                    component: 'views/components/workflows/component-based-step',
-                    componentname: 'component-based-step',
-                    externalstepdata: { 
-                        sitevisitedetailsstep: 'site-visit-details',
+                    informationboxdata: {
+                        heading: 'Workflow Complete: Review your work',
+                        text: 'Please review the summary information. You can go back to a previous step to make changes or "Quit Workflow" to discard your changes and start over',
                     },
                     layoutSections: [
                         {
@@ -171,32 +153,21 @@ define([
                                     componentName: 'site-visit-final-step',
                                     uniqueInstanceName: 'site-visit-final', /* unique to step */
                                     tilesManaged: 'none',
-                                    parameters: {},
+                                    parameters: {
+                                        graphid: '8d41e49e-a250-11e9-9eab-00224800b26d',
+                                        resourceid: "['site-visit-details']['site-visit-details']['resourceInstanceId']",
+                                        parenttileid: "['site-visit-details']['site-visit-details']['tileId']"
+                                    },
                                 },
                             ], 
                         },
                     ],
-                    graphid: '8d41e49e-a250-11e9-9eab-00224800b26d',
-                    nodegroupid: '',
-                    resourceid: null,
-                    tileid: null,
-                    parenttileid: null,
-                    icon: 'fa-check',
-                    nameheading: 'New Site Visit',
-                    namelabel: '[no label]',
-                    informationboxdata: {
-                        heading: 'Workflow Complete: Review your work',
-                        text: 'Please review the summary information. You can go back to a previous step to make changes or "Quit Workflow" to discard your changes and start over',
-                    }
                 }
 
             ];
 
             Workflow.apply(this, [params]);
             this.quitUrl = "/arches-her" + arches.urls.plugin('init-workflow');
-            self.getJSON('site-visit');
-            
-            self.ready(true);
         },
         template: { require: 'text!templates/views/components/plugins/site-visit.htm' }
     });
