c66aa03fb68053854b6ad7c809587d86f069ba0b
njkim <nkim@fargeo.com>
2021-11-23 11:58:46 -0800

    Update site-visit select consultation step, #709

--- arches_her/media/js/views/components/workflows/select-resource-step.js
+++ arches_her/media/js/views/components/workflows/select-resource-step.js
@@ -4,60 +4,45 @@ define([
     'arches',
     'knockout',
     'knockout-mapping',
-    'views/components/workflows/new-tile-step'
-], function(_, $, arches, ko, koMapping, NewTileStep) {
+], function(_, $, arches, ko, koMapping) {
     function viewModel(params) {
+        _.extend(this, params.form);
+
         var self = this;
-        NewTileStep.apply(this, [params]);
-        this.resValue = ko.observable();
-        if (!params.resourceid()) {
-            params.resourceid(ko.unwrap(params.workflow.resourceId));
-        }
-        if (params.workflow.steps[params._index]) {
-            params.tileid(ko.unwrap(params.workflow.steps[params._index].tileid));
-        }
-        this.disableResourceSelection = ko.observable(false);
-        if (ko.unwrap(params.workflow.resourceId)) {
-            this.resValue(ko.unwrap(params.workflow.resourceId));
-            this.disableResourceSelection(true);
-        }
-        this.loading(true);
-        this.graphid = params.graphid();
-        this.nameheading = params.nameheading;
-        this.namelabel = params.namelabel;
-        this.resValue.subscribe(function(val){
-            if (ko.unwrap(self.tile)) {
-                self.tile().resourceinstance_id = ko.unwrap(val);
+        this.graphids = params.graphids;
+        this.resourceValue = ko.observable();
+        this.resourceValue.subscribe(val => {
+            if (val){
+                self.tile().resourceinstance_id = val;
             }
-            params.resourceid(ko.unwrap(val));
-        }, this);
+        })
+        this.tile().transactionId = this.workflowId;
+        this.tile().dirty.subscribe(function(dirty) {
+            self.dirty(dirty)
+        });
 
-        this.card.subscribe(function(val){ if(ko.unwrap(val) != undefined) { this.loading(false); } }, this);
-        this.tile.subscribe(function(val){ if(ko.unwrap(val) != undefined) { this.loading(false); } }, this);
-        params.tile = self.tile;
-
-        this.setStateProperties = function(){
-            console.log("State Properties Updated")
-            params.workflow.steps[params._index] = params.defineStateProperties();
-            this.disableResourceSelection(true);
-        };
-
-        params.defineStateProperties = function(){
-            var wastebin = !!(ko.unwrap(params.wastebin)) ? koMapping.toJS(params.wastebin) : undefined;
-            var tileid = !!(ko.unwrap(params.tile)) ? ko.unwrap(params.tile().tileid): undefined;
-            var tile = !!(ko.unwrap(params.tile)) ? koMapping.toJS(params.tile().data) : undefined;
-            var completeTile = !!(ko.unwrap(params.tile)) ? ko.unwrap(params.tile).getData() : undefined;
-            if (wastebin && ko.unwrap(wastebin.hasOwnProperty('tile'))) {
-                wastebin.tile = completeTile;
+        this.initilize = function(){
+            if (ko.unwrap(self.savedData)) {
+                self.resourceValue(ko.unwrap(self.savedData).resourceInstanceId);
             }
-            ko.mapping.fromJS(wastebin, {}, params.wastebin);
-            return {
-                resourceid: ko.unwrap(params.resourceid),
-                tile: tile,
-                tileid: tileid,
-                wastebin: wastebin
-            };
+        }
+
+        params.form.save = function() {
+            self.tile().save().then(
+                function(){
+                    params.form.savedData({
+                        tileData: koMapping.toJSON(self.tile().data),
+                        resourceInstanceId: self.tile().resourceinstance_id,
+                        tileId: self.tile().tileid,
+                        nodegroupId: self.tile().nodegroup_id,
+                    });
+                    self.locked(true);
+                    params.form.complete(true);
+                    params.form.saving(false);
+                }
+            )
         };
+        this.initilize();
     }
 
     ko.components.register('select-resource-step', {
