d4d6516d360c5b4633725044edfa2c3c8f5e9b3a
Rob Oates <Rob.Oates@HistoricEngland.org.uk>
2021-12-09 15:07:38 +0000

    Define PREFERRED_COORDINATE_SYSTEMS and set default to BNG

--- arches_her/settings.py
+++ arches_her/settings.py
@@ -173,6 +173,11 @@ COPYRIGHT_TEXT = "All Rights Reserved."
 COPYRIGHT_YEAR = "2020"
 DOCKER = False
 
+PREFERRED_COORDINATE_SYSTEMS = (
+    {"name": "BNG","srid": "27700", "proj4": "+proj=tmerc +lat_0=49 +lon_0=-2 +k=0.9996012717 +x_0=400000 +y_0=-100000 +ellps=airy +datum=OSGB36 +units=m +no_defs", "default": True},
+    {"name": "LatLong", "srid": "4326", "proj4": "+proj=longlat +datum=WGS84 +no_defs", "default": False},  #Required
+)
+
 try:
     from .package_settings import *
 except ImportError:
