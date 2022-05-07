f12a9f27d5c590280acc81bbcaab98040acf9d85
Cyrus Hiatt <chiatt@fargeo.com>
2022-05-06 17:30:12 -0700

    Remove unused map sources, re #921

--- arches_her/pkg/preliminary_sql/select_sources.sql
+++ arches_her/pkg/preliminary_sql/select_sources.sql
@@ -3,15 +3,4 @@ INSERT INTO map_sources(name, source)
         "data": "/geojson?nodeid=1909956f-3a3b-11eb-ae99-f875a44e0e11&include_geojson_link=true",
         "type": "geojson"
     }');
-
-INSERT INTO map_sources(name, source)
-    VALUES ('select-area', '{
-        "data": "/geojson?nodeid=64be56e3-3ee5-11eb-b1f0-f875a44e0e11&include_geojson_link=true",
-        "type": "geojson"
-    }');
-
-INSERT INTO map_sources(name, source)
-    VALUES ('select-monument', '{
-        "data": "/geojson?nodeid=ca063178-28cf-11eb-be6d-f875a44e0e11&include_geojson_link=true",
-        "type": "geojson"
-    }');
+    
