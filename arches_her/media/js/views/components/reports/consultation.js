b0cd7044b0dd27f81d035c99debe99d741354a2f
Chantelle <chantelle.smith@HistoricEngland.org.uk>
2022-05-19 14:59:46 +0100

    Rename heritage asset to monument- consultation.js

--- arches_her/media/js/views/components/reports/consultation.js
+++ arches_her/media/js/views/components/reports/consultation.js
@@ -56,11 +56,11 @@ define([
             }
 
             self.resourcesDataConfig = {
-                assets: 'related heritage assets and areas',
+                assets: 'related monuments and areas',
                 files: 'file(s)',
                 relatedApplicationArea: 'consultation area'
             };
-            
+
             self.nameCards = {};
             self.locationCards = {};
             self.resourcesCards = {};
@@ -379,7 +379,7 @@ define([
                 self.resourcesCards = {
                     consultations: self.cards?.['associated consultations'],
                     activities: self.cards?.['associated activities'],
-                    assets: self.cards?.['associated heritage assets and areas'],
+                    assets: self.cards?.['associated monuments and areas'],
                     files: self.cards?.['associated digital files'],
                     relatedApplicationArea: self.cards?.['consultation location']
                 };
