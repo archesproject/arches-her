87f5b58947d1f9e8a9c58c905f1f873d308e9f0c
Andy Jones <andrew.jones@historicengland.org.uk>
2022-07-08 15:37:23 +0100

    adds initial xy input code

--- arches_her/media/js/views/components/search/bng-filter.js
+++ arches_her/media/js/views/components/search/bng-filter.js
@@ -15,8 +15,10 @@ define([
                 var self = this;
 
                 this.dependenciesLoaded = ko.observable(false)
-                this.bng = ko.observable("").extend({ rateLimit: 200 });
-                this.buffer = ko.observable(0).extend({ rateLimit: 200 });
+                this.bng = ko.observable("");
+                this.bng_x = ko.observable(0);
+                this.bng_y = ko.observable(0);
+                this.buffer = ko.observable(0);
                 this.mapboxgl = mapboxgl;
 
                 options.name = "BNG Filter";
@@ -30,24 +32,41 @@ define([
                 
                 this.bngMessageError = ko.observable("");
                 this.bufferMessageError = ko.observable("");
+                this.xyMessageError = ko.observable("");
+
                 this.bngHasError = ko.computed(function() {
-                    console.log(ko.unwrap(self.bngMessageError()));
                     return self.bngMessageError().length > 0;
                 });
                 this.bufferHasError = ko.computed(function() {
-                    console.log(ko.unwrap(self.bufferMessageError()));
                     return self.bufferMessageError().length > 0;
+                });
+                this.xyHasError = ko.computed(function() {
+                    return self.xyMessageError().length > 0;
                 });
 
                 this.bng.subscribe(function(value) {
                     self.validate();
                     let hasErr = ko.unwrap(self.bngHasError());
                     if(!hasErr){
+                        let bng_xy = self.xyFromBng(value);
+                        self.bng_x = bng_xy["x"];
+                        self.bng_y = bng_xy["y"];
                         self.filter.bng(value);
                         self.updateQuery();
                     }
-                    
                 });
+
+                this.bng_x.subscribe(function(value) {
+                    self.validate();
+                    let hasErr = ko.unwrap(self.xyHasError());
+                    if(!hasErr){
+                        let bng = self.bngFromXy(value, self.bng_y());
+                        self.bng(bng);
+                        self.filter.bng(bng);
+                        self.updateQuery();
+                    }
+                });
+
                 this.buffer.subscribe(function(value) {
                     self.validate();
                     let hasErr = ko.unwrap(self.bufferHasError());
@@ -57,13 +76,94 @@ define([
                     }
                 });
 
-                this.gridList = [
+                /*this.gridList = [
                     "HO", "HP","HT","HU","HW","HX","HY","HZ",
                     "NA","NB","NC","ND","NE","NF","NG","NH","NJ","NK","NL","NM","NN","NO","NP","NQ","NR","NS","NT","NU","NV","NW","NX","NY","NZ",
                     "OA","OB","OF","OG","OL","OM","OQ","OR","OV","OW","SA","SB",
                     "SC","SD","SE","SF","SG","SH","SJ","SK","SL","SM","SN","SO","SP","SQ","SR","SS","ST","SU","SV","SW","SX","SY","SZ",
                     "TA","TB","TF","TG","TL","TM","TQ","TR","TV","TW"
-                ]
+                ]*/
+
+                this.gridList = {
+                    "HO": [3, 12],
+                    "HP": [4, 12],
+                    "HT": [3, 11],
+                    "HU": [4, 11],
+                    "HW": [1, 10], 
+                    "HX": [2, 10],
+                    "HY": [3, 10],
+                    "HZ": [4, 10],
+                    "NA": [0, 9],
+                    "NB": [1, 9],
+                    "NC": [2, 9],
+                    "ND": [3, 9],
+                    "NE": [4, 9],
+                    "OA": [5, 9],
+                    "OB": [6, 9],
+                    "NF": [0, 8],
+                    "NG": [1, 8],
+                    "NH": [2, 8],
+                    "NJ": [3, 8],
+                    "NK": [4, 8],
+                    "OF": [5, 8],
+                    "OG": [6, 8],
+                    "NL": [0, 7],
+                    "NM": [1, 7],
+                    "NN": [2, 7],
+                    "NO": [3, 7],
+                    "NP": [4, 7],
+                    "OL": [5, 7],
+                    "OM": [6, 7],
+                    "NQ": [0, 6],
+                    "NR": [1, 6],
+                    "NS": [2, 6],
+                    "NT": [3, 6],
+                    "NU": [4, 6],
+                    "OQ": [5, 6],
+                    "OR": [6, 6],
+                    "NV": [0, 5],
+                    "NW": [1, 5],
+                    "NX": [2, 5],
+                    "NY": [3, 5],
+                    "NZ": [4, 5],
+                    "OV": [5, 5],
+                    "OW": [6, 5],
+                    "SA": [0, 4],
+                    "SB": [1, 4],
+                    "SC": [2, 4],
+                    "SD": [3, 4],
+                    "SE": [4, 4],
+                    "TA": [5, 4],
+                    "TB": [6, 4],
+                    "SF": [0, 3],
+                    "SG": [1, 3],
+                    "SH": [2, 3],
+                    "SJ": [3, 3],
+                    "SK": [4, 3],
+                    "TF": [5, 3],
+                    "TG": [6, 3],
+                    "SL": [0, 2],
+                    "SM": [1, 2],
+                    "SN": [2, 2],
+                    "SO": [3, 2],
+                    "SP": [4, 2],
+                    "TL": [5, 2],
+                    "TM": [6, 2],
+                    "SQ": [0, 1],
+                    "SR": [1, 1],
+                    "SS": [2, 1],
+                    "ST": [3, 1],
+                    "SU": [4, 1],
+                    "TQ": [5, 1],
+                    "TR": [6, 1],
+                    "SV": [0, 0],
+                    "SW": [1, 0],
+                    "SX": [2, 0],
+                    "SY": [3, 0],
+                    "SZ": [4, 0],
+                    "TV": [5, 0],
+                    "TW": [6, 0]
+                }
 
                 this.validateBng = function() {
                     self.bngMessageError("");
@@ -81,7 +181,7 @@ define([
                         }
                         
                         // the first tochars must be in the gridlist
-                        var gridlist = self.gridList;
+                        var gridlist = Object.keys(self.gridList);
                         if (gridlist.indexOf(self.bng().toUpperCase().substring(0, 2)) === -1) {
                             self.bngMessageError("BNG must start with a valid 100km grid square identifier");
                             return;
@@ -97,6 +197,15 @@ define([
 
                     }
                 }               
+
+                this.validateXy = function() {
+                    self.xyMessageError("");
+                    if (!self.bng_x() || isNaN(self.bng_x) || self.bng_x() > -1 || !self.bng_y() || isNaN(self.bng_y) || !self.bng_y() > -1) {
+
+                        self.xyMessageError("X and Y must be numeric 0 or greater");
+                        return;
+                    }
+                }
 
                 this.validateBuffer = function() {
                     var buffer = self.buffer();
@@ -119,11 +228,45 @@ define([
                     self.bufferMessageError("");
                     return;
                 }
-
+                
+                //validate the inputs and
                 this.validate = function() {
                     self.validateBng();
                     self.validateBuffer();
                 }
+
+                this.xyFromBng = function(bng) {
+                    let ret = {"x":0,"y":0};
+                    try {
+                        if(bng.length() = 12) {
+                            let oneHundredKmGrid = self.gridList(bng.substring(0, 2));
+                            let x = oneHundredKmGrid[0] * 100000 + parseInt(bng.substring(2, bng.length / 2));
+                            let y = oneHundredKmGrid[1] * 100000 + parseInt(bng.substring(bng.length / 2, bng.length));
+                            ret = {"x":x,"y":y};
+                        }
+                    } catch (error) {
+                        self.xyMessageError("Could not convert BNG to XY");
+                    }
+                    
+                    return ret;
+                }
+
+                this.bngFromXY = function(x,y) {
+                    let bng = "";
+                    try {
+                        let gridlist = self.gridList;
+                        let xOneHundredKmGrid = Math.floor(x / 100000);
+                        let yOneHundredKmGrid = Math.floor(y / 100000);
+                        let xGrid = Math.floor((x % 100000) / 1000);
+                        let yGrid = Math.floor((y % 100000) / 1000);
+                        bng = gridlist[xOneHundredKmGrid].toString() + gridlist[yOneHundredKmGrid].toString() + xGrid.toString() + yGrid.toString();
+                    }
+                    catch (error) {
+                        self.xyMessageError("Could not convert XY to BNG");
+                    }
+                    return bng;
+                }
+
 
                 this.filters[componentName](this);
                 this.pageLoaded = false;
@@ -174,6 +317,7 @@ define([
                         }
                     }
                 ]);
+
                 this.layers.unshift({
                     "id": "background-fill",
                     "type": "background",
@@ -181,6 +325,7 @@ define([
                         "background-color": "#f2f2f2"
                     }
                 });
+
                 this.sprite = arches.mapboxSprites;
                 this.glyphs = arches.mapboxGlyphs;
                 this.sources = Object.assign({
