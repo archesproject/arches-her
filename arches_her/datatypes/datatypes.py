408c190afbb824705cbd291176e4828774622395
Kingsley Hodgkinson <Kingsley.Hodgkinson@HistoricEngland.org.uk>
2022-07-21 15:34:27 +0100

    Expand Across All graphs using PRN in Keystone.

--- arches_her/datatypes/datatypes.py
+++ arches_her/datatypes/datatypes.py
@@ -48,9 +48,27 @@ class NumberDataType(datatypes.NumberDataType):
    
     def append_to_document(self, document, nodevalue, nodeid, tile, provisional=False):
         ## #46273 Primary Referance Number Override.
-        ## - Add Monument.PrimaryRefNo["325a2f33-efe4-11eb-b0bb-a87eeabdefba"] to document.strings for quicksearch.
+        ## - Add PrimaryRefNo to document.strings for quicksearch.
+        prime_refno_list = ["e7d69602-9939-11ea-b514-f875a44e0e11", 	## "Activity"
+                            "3bdc39fb-9a93-11ea-b4fe-f875a44e0e11", 	## "Archive Source"
+                            "8dca12b3-edeb-11eb-a9ee-a87eeabdefba", 	## "Area"
+                            "dd8032af-b494-11ea-8110-f875a44e0e11", 	## "Artefact"
+                            "46fbf790-95ce-11ea-8331-f875a44e0e11", 	## "Bibliographic Source"
+                            "b37552bd-9527-11ea-97f4-f875a44e0e11", 	## "Consultation"
+                            "79026215-a0f4-11ea-81df-f875a44e0e11", 	## "Digital Object"
+                            "44441e0c-99ac-11ea-97cc-f875a44e0e11", 	## "Heritage Story"
+                            "7f5591c5-efed-11eb-8e44-a87eeabdefba", 	## "Historic Aircraft"
+                            "4a1e7dc7-f000-11eb-ac44-a87eeabdefba", 	## "Historic Landscape Characterization"
+                            "f1cbd897-f007-11eb-8b4b-a87eeabdefba", 	## "Maritime Vessel"
+                            "325a2f33-efe4-11eb-b0bb-a87eeabdefba", 	## "Monument"
+                            "c8e9e9c4-9456-11ea-a001-f875a44e0e11", 	## "Organization"
+                            "1f7ada20-eff5-11eb-b62a-a87eeabdefba", 	## "Period"
+                            "9718f941-950e-11ea-a048-f875a44e0e11", 	## "Person"
+                            "9d8fc930-eff6-11eb-a991-a87eeabdefba", 	## "Place"
+                            "d6734e3d-94fb-11ea-b836-f875a44e0e11"  	## "Application Area"
+                            ]
         try:
-            if str(nodeid) == "325a2f33-efe4-11eb-b0bb-a87eeabdefba" :
+            if str(nodeid) in prime_refno_list :
                 val = {"string": str(nodevalue), "nodegroup_id": tile.nodegroup_id, "provisional": provisional}
                 document["strings"].append(val)
         except Exception as e:
