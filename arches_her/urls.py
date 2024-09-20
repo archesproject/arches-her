d1c8f42c7316f32f3644d20ecfb890fc983ad3ed
Rob Gaston <rgaston@fargeo.com>
2024-09-20 12:07:13 -0700

    extends index doc apis

--- arches_her/urls.py
+++ arches_her/urls.py
@@ -6,7 +6,7 @@ from arches.app.views.plugin import PluginView
 from arches_her.views.file_template import FileTemplateView
 from arches_her.views.active_consultations import ActiveConsultationsView
 from arches_her.views.index import IndexView
-from arches_her.views.datatype_document import DatatypeDocumentView
+from arches_her.views.datatype_document import DatatypeDocumentView, ResourceDocumentView
 
 uuid_regex = settings.UUID_REGEX
 
@@ -24,7 +24,13 @@ urlpatterns = [
     re_path(r'^plugins/communication-workflow', PluginView.as_view(), name='communication-workflow'),
     re_path(r'^plugins/init-workflow', PluginView.as_view(), name='init-workflow'),
     re_path(
-        r"^datatype_document/(?P<tileid>%s)/(?P<nodeid>%s)$" % (uuid_regex, uuid_regex), DatatypeDocumentView.as_view(), name="datatype_document"
+        r"^datatype_document/(?P<tileid>%s)$" % uuid_regex, DatatypeDocumentView.as_view(), name="datatype_document"
+    ),
+    re_path(
+        r"^datatype_document/(?P<tileid>%s)/(?P<nodeid>%s|())$" % (uuid_regex, uuid_regex), DatatypeDocumentView.as_view(), name="datatype_document"
+    ),
+    re_path(
+        r"^resource_document/(?P<resourceid>%s)$" % uuid_regex, ResourceDocumentView.as_view(), name="resource_document"
     ),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
--- arches_her/views/datatype_document.py
+++ arches_her/views/datatype_document.py
@@ -2,11 +2,12 @@ from arches.app.views.api import APIBase
 from arches.app.models.models import Node, TileModel
 from arches.app.datatypes.datatypes import DataTypeFactory
 from arches.app.utils.response import JSONResponse
+from arches.app.models.resource import Resource
 
 
 class DatatypeDocumentView(APIBase):
 
-    def get(self, request, tileid=None, nodeid=None):
+    def get(self, request, tileid, nodeid=''):
         document = {
             "resourceinstanceid": '',
             "graph_id": '',
@@ -29,9 +30,30 @@ class DatatypeDocumentView(APIBase):
         }
 
         datatype_factory = DataTypeFactory()
-        node = Node.objects.get(pk=nodeid)
         tile = TileModel.objects.get(pk=tileid)
-        datatype_instance = datatype_factory.get_instance(node.datatype) 
-        datatype_instance.append_to_document(document, tile.data[nodeid], nodeid, tile)
+        nodes = Node.objects.filter(nodegroup=tile.nodegroup)
+        if nodeid is not '':
+            nodes = nodes.filter(pk=nodeid)
+        for node in nodes:
+            datatype = node.datatype
+            nodeid = str(node.pk)
+            try:
+                value = tile.data[nodeid]
+                if value not in ("", [], {}, None):
+                    datatype_instance = datatype_factory.get_instance(datatype)
+                    datatype_instance.append_to_document(document, value, nodeid, tile)
+            except KeyError:
+                pass
+        return JSONResponse(document)
 
+
+class ResourceDocumentView(APIBase):
+
+    def get(self, request, resourceid):
+        resource = Resource.objects.get(pk=resourceid)
+        datatype_factory = DataTypeFactory()
+        node_datatypes = {str(nodeid): datatype for nodeid, datatype in Node.objects.values_list("nodeid", "datatype")}
+        document, terms = resource.get_documents_to_index(
+            fetchTiles=True, datatype_factory=datatype_factory, node_datatypes=node_datatypes
+        )
         return JSONResponse(document)
