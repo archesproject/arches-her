ab16374aa4661b5656fc2996d86d793d508bb0e6
Rob Gaston <rgaston@fargeo.com>
2024-09-20 21:53:22 -0700

    nits

--- arches_her/views/index_document.py
+++ arches_her/views/index_document.py
@@ -4,31 +4,33 @@ from arches.app.datatypes.datatypes import DataTypeFactory
 from arches.app.utils.response import JSONResponse
 from arches.app.models.resource import Resource
 
+class IndexDocument():
+
+    def __init__(self):
+        self.resourceinstanceid = ''
+        self.graph_id = ''
+        self.map_popup = ''
+        self.displayname = ''
+        self.root_ontology_class = ''
+        self.legacyid = ''
+        self.displayname = []
+        self.displaydescription = []
+        self.tiles = []
+        self.permissions = []
+        self.strings = []
+        self.dates = []
+        self.domains = []
+        self.geometries = []
+        self.points = []
+        self.numbers = []
+        self.date = []
+        self.ids = []
+
 
 class DatatypeDocumentView(APIBase):
 
     def get(self, request, tileid, nodeid=''):
-        document = {
-            "resourceinstanceid": '',
-            "graph_id": '',
-            "map_popup": '',
-            "displayname": '',
-            "root_ontology_class": '',
-            "legacyid": '',
-            "displayname": [],
-            "displaydescription": [],
-            "tiles": [],
-            "permissions": [],
-            "strings": [],
-            "dates": [],
-            "domains": [],
-            "geometries": [],
-            "points": [],
-            "numbers": [],
-            "date": [],
-            "ids": [],
-        }
-
+        document = IndexDocument()
         datatype_factory = DataTypeFactory()
         tile = TileModel.objects.get(pk=tileid)
         nodes = Node.objects.filter(nodegroup=tile.nodegroup)
@@ -41,7 +43,7 @@ class DatatypeDocumentView(APIBase):
                 value = tile.data[nodeid]
                 if value not in ("", [], {}, None):
                     datatype_instance = datatype_factory.get_instance(datatype)
-                    datatype_instance.append_to_document(document, value, nodeid, tile)
+                    datatype_instance.append_to_document(document.__dict__, value, nodeid, tile)
             except KeyError:
                 pass
         return JSONResponse(document)
@@ -52,8 +54,9 @@ class ResourceDocumentView(APIBase):
     def get(self, request, resourceid):
         resource = Resource.objects.get(pk=resourceid)
         datatype_factory = DataTypeFactory()
-        node_datatypes = {str(nodeid): datatype for nodeid, datatype in Node.objects.values_list("nodeid", "datatype")}
-        document, terms = resource.get_documents_to_index(
+        nodes = Node.objects.filter(graph_id=resource.graph_id)
+        node_datatypes = {str(nodeid): datatype for nodeid, datatype in nodes.values_list("nodeid", "datatype")}
+        document = resource.get_documents_to_index(
             fetchTiles=True, datatype_factory=datatype_factory, node_datatypes=node_datatypes
         )
         return JSONResponse(document)
