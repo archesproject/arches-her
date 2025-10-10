ef130e09ba80d9cbf074c326dcb3de2478a5af2c
Samuel Scandrett <samuel.scandrett@k-int.co.uk>
2025-10-10 15:00:01 +0100

    Change to SQL migration

--- arches_her/migrations/0004_update_plugins.py
+++ arches_her/migrations/0004_update_plugins.py
@@ -6,28 +6,21 @@ class Migration(migrations.Migration):
 
     dependencies = [("arches_her", "0003_add_bng_search_view")]
 
-    def update_plugins(apps, scheme_editor):
-        Plugin = apps.get_model("models", "Plugin")
+    # accessibility plugin = '8688eaf9-3605-4384-823c-d01c0d210f82'
+    # active consultations = '76a27df9-6f16-47ed-bd47-bb95c0fe7173'
 
-        active_consultations = Plugin.objects.get(pluginid="76a27df9-6f16-47ed-bd47-bb95c0fe7173")
-        active_consultations.config["show"] = True
-        active_consultations.save()
+    add_plugin_show_true = """
+        UPDATE plugins 
+        SET config = jsonb_set(config, '{show}', 'true'::jsonb, true) 
+        WHERE pluginid in ('76a27df9-6f16-47ed-bd47-bb95c0fe7173','8688eaf9-3605-4384-823c-d01c0d210f82');
+        """
 
-        accessibility_statement = Plugin.objects.get(pluginid="8688eaf9-3605-4384-823c-d01c0d210f82")
-        accessibility_statement.config["show"] = True
-        accessibility_statement.save()
-
-    def remove_plugin_updates(apps, scheme_editor):
-        Plugin = apps.get_model("models", "Plugin")
-
-        active_consultations = Plugin.objects.get(pluginid="76a27df9-6f16-47ed-bd47-bb95c0fe7173")
-        active_consultations.config.pop("show", None)
-        active_consultations.save()
-
-        accessibility_statement = Plugin.objects.get(pluginid="8688eaf9-3605-4384-823c-d01c0d210f82")
-        accessibility_statement.config.pop("show", None)
-        accessibility_statement.save()
+    remove_plugin_show = """
+        UPDATE plugins 
+        SET config = config - 'show' 
+        WHERE pluginid in ('76a27df9-6f16-47ed-bd47-bb95c0fe7173','8688eaf9-3605-4384-823c-d01c0d210f82');
+        """
 
     operations = [
-        migrations.RunPython(update_plugins, remove_plugin_updates),
+        migrations.RunSQL(add_plugin_show_true, remove_plugin_show),
     ]
