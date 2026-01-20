2cf006c69ec2a31a898bedac0550a6efa081190c
khodgkinson-he <53860784+khodgkinson-he@users.noreply.github.com>
2026-01-20 16:08:42 +0000

    Enhance referenced-by component: improve graphs handling and update template for better visibility of relationships

--- arches_her/media/js/views/components/reports/scenes/referenced-by.js
+++ arches_her/media/js/views/components/reports/scenes/referenced-by.js
@@ -13,7 +13,7 @@ function($, _, ko, arches, reportUtils, ReferencedByTemplate) {
             const self = this;
             Object.assign(self, reportUtils);
             self.resourceinstanceid = params.resourceInstanceId;
-            self.graphsArray = params.graphs;
+            self.graphsArray = params.graphs || [];
             self.graphsList = [];
             self.graphs = [];
             self.relations = ko.observableArray();
@@ -55,12 +55,24 @@ function($, _, ko, arches, reportUtils, ReferencedByTemplate) {
                 })
                     .done(function(response) {
                         self.getGraphs().then(function(){
-                            var responseRelatedResources = response.related_resources.related_resources;
+                            let this_rid = response.related_resources.resource_instance.resourceinstanceid;
+                            //need to look at the resource_relationships and find other resources that are pointing TO this one
+                            other_resource_ids = [];
+                            for(const rr of response.related_resources.resource_relationships){
+                                if(rr.resourceinstanceidto == self.resourceinstanceid){
+                                    other_resource_ids.push(rr.resourceinstanceidfrom)
+                            }
+                            }
+                            // we now have the resources that point at this one.
+                            var responseRelatedResources = response.related_resources.related_resources.filter((x) => other_resource_ids.includes(x.resourceinstanceid));
                             self.relations.removeAll();
                             for(let r in responseRelatedResources){
                                 var responseRelatedResource = responseRelatedResources[r];
-                                if(self.graphsList.includes(responseRelatedResource["graph_id"] )){
-                                    var graphName = (self.graphs.find((gr) => gr.graphid === responseRelatedResource["graph_id"]))["name"];
+                                if(
+                                    self.graphsList.length === 0 || self.graphsList.includes(responseRelatedResource["graph_id"] )){
+                                    
+                                    var graphObj = self.graphs.find((gr) => gr.graphid === responseRelatedResource["graph_id"]);
+                                    var graphName = graphObj ? graphObj.name : "unknown";
                                     self.relations.push({"related_resource_name": responseRelatedResource["displayname"], "related_resource_link": arches.urls.resource_report + responseRelatedResource["resourceinstanceid"], "related_resource_type": graphName});
                                 }
                             }
