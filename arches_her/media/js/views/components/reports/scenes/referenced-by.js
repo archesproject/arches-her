2dced8864c04ff035ee15ff033ce8f7998e9177e
khodgkinson-he <53860784+khodgkinson-he@users.noreply.github.com>
2026-01-20 16:16:47 +0000

    Refactor getRelatedResources function: streamline resource filtering and improve readability

--- arches_her/media/js/views/components/reports/scenes/referenced-by.js
+++ arches_her/media/js/views/components/reports/scenes/referenced-by.js
@@ -48,41 +48,45 @@ function($, _, ko, arches, reportUtils, ReferencedByTemplate) {
             };
 
 
-            self.getRelatedResources = function(){
+             self.getRelatedResources = function(){
                 return $.ajax({
                     url: arches.urls.related_resources + self.resourceinstanceid,
                     context: self,
                 })
-                    .done(function(response) {
-                        self.getGraphs().then(function(){
-                            let this_rid = response.related_resources.resource_instance.resourceinstanceid;
-                            //need to look at the resource_relationships and find other resources that are pointing TO this one
-                            other_resource_ids = [];
-                            for(const rr of response.related_resources.resource_relationships){
-                                if(rr.resourceinstanceidto == self.resourceinstanceid){
-                                    other_resource_ids.push(rr.resourceinstanceidfrom)
+                .done(function(response) {
+                    self.getGraphs().then(function(){
+                        const relationships = response.related_resources.resource_relationships;
+                        const relatedResources = response.related_resources.related_resources;
+                        // Collect resource IDs that point to this resource
+                        const otherResourceIds = relationships
+                            .filter(rr => rr.resourceinstanceidto === self.resourceinstanceid)
+                            .map(rr => rr.resourceinstanceidfrom);
+
+                        // Filter related resources that reference this one
+                        const filteredResources = relatedResources.filter(x => otherResourceIds.includes(x.resourceinstanceid));
+
+                        self.relations.removeAll();
+                        filteredResources.forEach(resource => {
+                            if (
+                                self.graphsList.length === 0 ||
+                                self.graphsList.includes(resource.graph_id)
+                            ) {
+                                const graphObj = self.graphs.find(gr => gr.graphid === resource.graph_id);
+                                const graphName = graphObj ? graphObj.name : "unknown";
+                                self.relations.push({
+                                    related_resource_name: resource.displayname,
+                                    related_resource_link: arches.urls.resource_report + resource.resourceinstanceid,
+                                    related_resource_type: graphName
+                                });
                             }
-                            }
-                            // we now have the resources that point at this one.
-                            var responseRelatedResources = response.related_resources.related_resources.filter((x) => other_resource_ids.includes(x.resourceinstanceid));
-                            self.relations.removeAll();
-                            for(let r in responseRelatedResources){
-                                var responseRelatedResource = responseRelatedResources[r];
-                                if(
-                                    self.graphsList.length === 0 || self.graphsList.includes(responseRelatedResource["graph_id"] )){
-                                    
-                                    var graphObj = self.graphs.find((gr) => gr.graphid === responseRelatedResource["graph_id"]);
-                                    var graphName = graphObj ? graphObj.name : "unknown";
-                                    self.relations.push({"related_resource_name": responseRelatedResource["displayname"], "related_resource_link": arches.urls.resource_report + responseRelatedResource["resourceinstanceid"], "related_resource_type": graphName});
-                                }
-                            }
-                            return;
                         });
-                    })
-                    .fail(function() {
-                        // error
                     });
+                })
+                .fail(function() {
+                    // error
+                });
             };
+
 
             self.getRelatedResources();
         },
