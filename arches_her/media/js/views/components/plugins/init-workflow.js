8fa1f4c4569d4d8a7634007f13946a6aef3a02da
Samuel Scandrett <samuel.scandrett@k-int.co.uk>
2026-02-25 12:26:21 +0000

    Add incomplete workflow and help code from AfS

--- arches_her/media/js/views/components/plugins/init-workflow.js
+++ arches_her/media/js/views/components/plugins/init-workflow.js
@@ -1,14 +1,86 @@
 define([
     'knockout',
     'arches',
+    'viewmodels/alert-json',
     'templates/views/components/plugins/init-workflow.htm'
-], function(ko, arches, InitWorkflowTemplate) {
+], function(ko, arches, JsonErrorAlertViewModel, InitWorkflowTemplate) {
 
     var InitWorkflow = function(params) {
+        this.workflows = ko.observableArray([]);
+        this.helpTemplateData = ko.observableArray([]);
+
         this.workflows = params.workflows.map(function(wf){
             wf.url = arches.urls.plugin(wf.slug);
             return wf;
         }, this);
+
+        // fetch(arches.urls.api_plugins).then(resp => {
+        //     if (resp.ok) {
+        //         return resp.json();
+        //     }
+        //     else {
+        //         params.alert(new JsonErrorAlertViewModel('ep-alert-red', resp.responseJSON));
+        //     }
+        // }).then(respJSON => {
+        //     let workflows = respJSON.reduce((acc, plugin) => {
+        //         if (plugin.config.is_workflow) {
+        //             plugin.url = arches.urls.plugin(plugin.slug);
+        //             acc.push(plugin);
+        //         }
+        //         return acc;
+        //     }, []);
+
+        //     this.workflows(workflows);
+        //     this.helpTemplateData(workflows.reduce((acc, workflow) => {
+        //         if (workflow.helptemplate) {
+        //             acc.push({'text': workflow.name, 'id': workflow.helptemplate});
+        //         }
+
+        //         return acc;
+        //     }, []));
+        // });
+
+        this.shouldShowWorkflowHelp = ko.observable(false);
+        this.helpTemplateUrl = ko.observable();
+        this.isHelpTemplateLoading = ko.observable();
+        this.selectedHelpTemplate = ko.observable();
+        this.selectedHelpTemplate.subscribe(helpTemplateName => {
+            if (helpTemplateName) {
+                this.isHelpTemplateLoading(true);
+                this.helpTemplateUrl(arches.urls.help_template + `?template=${helpTemplateName}`);
+            }
+            else {
+                this.helpTemplateUrl(null);
+            }
+        })
+
+        this.shouldShowIncompleteWorkflowsModal = ko.observable(false);
+        this.requestingUserIsSuperuser = ko.observable(false);
+
+        this.incompleteWorkflows = ko.observableArray([]);
+        this.incompleteWorkflows.subscribe(incompleteWorkflows => {
+            if (incompleteWorkflows.length) {
+                this.shouldShowIncompleteWorkflowsModal(true);
+            }
+        });
+
+        fetch(arches.urls.api_user_incomplete_workflows).then(resp => {
+            if (resp.ok) {
+                return resp.json();
+            }
+            else {
+                params.alert(new JsonErrorAlertViewModel('ep-alert-red', resp.responseJSON));
+            }
+        }).then(respJSON => {
+            this.incompleteWorkflows(respJSON['incomplete_workflows'].map(workflowData => {
+                const datetime = new Date(workflowData['created']);
+                workflowData['created'] = datetime.toLocaleString();
+
+                return workflowData;
+            }));
+
+            this.requestingUserIsSuperuser(respJSON['requesting_user_is_superuser']);
+       });
     };
 
     return ko.components.register('init-workflow', {
