f0be70ae5e30a90d71055dd342a4c2b4bbfb8f04
Samuel Scandrett <samuel.scandrett@k-int.co.uk>
2026-02-26 13:44:09 +0000

    Minor formatting to existing 0004 migration

--- arches_her/migrations/0004_update_plugins.py
+++ arches_her/migrations/0004_update_plugins.py
@@ -6,21 +6,22 @@ class Migration(migrations.Migration):
 
     dependencies = [("arches_her", "0003_add_bng_search_view")]
 
-    # accessibility plugin = '8688eaf9-3605-4384-823c-d01c0d210f82'
-    # active consultations = '76a27df9-6f16-47ed-bd47-bb95c0fe7173'
+    accessibility_plugin = '8688eaf9-3605-4384-823c-d01c0d210f82'
+    active_consultations = '76a27df9-6f16-47ed-bd47-bb95c0fe7173'
 
-    add_plugin_show_true = """
+    add_plugin_show_true_accessibility_activecons = """
         UPDATE plugins 
         SET config = jsonb_set(config, '{show}', 'true'::jsonb, true) 
-        WHERE pluginid in ('76a27df9-6f16-47ed-bd47-bb95c0fe7173','8688eaf9-3605-4384-823c-d01c0d210f82');
-        """
+        WHERE pluginid in ('%s','%s');
+        """ % (active_consultations, accessibility_plugin)
 
-    remove_plugin_show = """
+    remove_plugin_show_accessibility_activecons = """
         UPDATE plugins 
         SET config = config - 'show' 
-        WHERE pluginid in ('76a27df9-6f16-47ed-bd47-bb95c0fe7173','8688eaf9-3605-4384-823c-d01c0d210f82');
-        """
+        WHERE pluginid in ('%s','%s');
+        """ % (active_consultations, accessibility_plugin)
+
 
     operations = [
-        migrations.RunSQL(add_plugin_show_true, remove_plugin_show),
+        migrations.RunSQL(add_plugin_show_true_accessibility_activecons, remove_plugin_show_accessibility_activecons),
     ]
