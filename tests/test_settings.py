894c1f301bbe1c411666c16d34b07506f3da44ba
CWDamm-Kint <chris.damm@k-int.co.uk>
2026-02-27 15:04:20 +0000

    add ontology path to test-settings

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -72,6 +72,8 @@ SILENCED_SYSTEM_CHECKS.append(
 
 ELASTICSEARCH_HOSTS = [{"scheme": "http", "host": "localhost", "port": ELASTICSEARCH_HTTP_PORT}]
 
+ONTOLOGY_PATH = os.path.join(PROJECT_TEST_ROOT, "fixtures", "ontologies", "cidoc_crm")
+
 try:
     from arches_her.settings_local import *
 except ImportError:
