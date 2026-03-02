0e82dd18cfa30732be879fbdb846a0c136613d64
Samuel Scandrett <samuel.scandrett@k-int.co.uk>
2026-03-02 21:18:32 +0000

    Format 0004 migration file

--- arches_her/migrations/0004_update_plugins.py
+++ arches_her/migrations/0004_update_plugins.py
@@ -6,20 +6,26 @@ class Migration(migrations.Migration):
 
     dependencies = [("arches_her", "0003_add_bng_search_view")]
 
-    accessibility_plugin = '8688eaf9-3605-4384-823c-d01c0d210f82'
-    active_consultations = '76a27df9-6f16-47ed-bd47-bb95c0fe7173'
+    accessibility_plugin = "8688eaf9-3605-4384-823c-d01c0d210f82"
+    active_consultations = "76a27df9-6f16-47ed-bd47-bb95c0fe7173"
 
     add_plugin_show_true_accessibility_activecons = """
         UPDATE plugins 
         SET config = jsonb_set(config, '{show}', 'true'::jsonb, true) 
         WHERE pluginid in ('%s','%s');
-        """ % (active_consultations, accessibility_plugin)
+        """ % (
+        active_consultations,
+        accessibility_plugin,
+    )
 
     remove_plugin_show_accessibility_activecons = """
         UPDATE plugins 
         SET config = config - 'show' 
         WHERE pluginid in ('%s','%s');
-        """ % (active_consultations, accessibility_plugin)
+        """ % (
+        active_consultations,
+        accessibility_plugin,
+    )
 
     def workflow_config_update(apps, scheme_editor):
         Plugin = apps.get_model("models", "Plugin")
@@ -30,12 +36,13 @@ class Migration(migrations.Migration):
         for wf_config in init_workflow_plugin.config["workflows"]:
             inner_worfklow_plugin = Plugin.objects.get(pluginid=wf_config["workflowid"])
 
-            inner_worfklow_plugin.config = {"show": False,
-                                            "is_workflow": True,
-                                            "description": wf_config["desc"],
-                                            "thumbnailBackgroundColor": wf_config["bgColor"],
-                                            "thumbnailCircleColor": wf_config["circleColor"],
-                                            }
+            inner_worfklow_plugin.config = {
+                "show": False,
+                "is_workflow": True,
+                "description": wf_config["desc"],
+                "thumbnailBackgroundColor": wf_config["bgColor"],
+                "thumbnailCircleColor": wf_config["circleColor"],
+            }
             inner_worfklow_plugin.save()
 
         # clear list from init-workflow (del list produces error)
@@ -165,7 +172,6 @@ class Migration(migrations.Migration):
         site_visit = Plugin.objects.get(pluginid="0b1499e0-6cdc-403b-a2e3-499c2201069d")
         site_visit.helptemplate = None
         site_visit.save()
-
 
     operations = [
         migrations.RunSQL(add_plugin_show_true_accessibility_activecons, remove_plugin_show_accessibility_activecons),
