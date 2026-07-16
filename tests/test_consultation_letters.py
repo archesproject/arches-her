396f9f86e559e020a373a4377fb3d162f735b332
Samuel Scandrett <samuel.scandrett@k-int.co.uk>
2026-07-16 14:54:48 +0100

    Formatting nit

--- tests/test_consultation_letters.py
+++ tests/test_consultation_letters.py
@@ -1,5 +1,6 @@
 from arches_her.utils.file_template_modifier import FileTemplateModifier
 
+
 class TestFileTemplateModifier(FileTemplateModifier):
 
     def __init__(self):
@@ -9,5 +10,6 @@ class TestFileTemplateModifier(FileTemplateModifier):
     def get_template_path():
         return {
             "135d243a-3b91-4cb4-9bd7-9b37e1a974d0": "Test1.docx",
-            "730583db-eef1-4913-b0de-84e03a19f75e": "Test2.docx"
+            "730583db-eef1-4913-b0de-84e03a19f75e": "Test2.docx",
+            "e80f8344-cea8-4d30-8c9a-c7e86b1e6d44": "Test3.docx",
         }
