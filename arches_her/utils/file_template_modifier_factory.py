be4f1ed89b6e0ae61f2262a6fb2d79d1d096398e
Samuel Scandrett <samuel.scandrett@k-int.co.uk>
2026-07-16 10:11:23 +0100

    Creates a factory and base file template modifier

---
+++ arches_her/utils/file_template_modifier_factory.py
@@ -0,0 +1,31 @@
+from arches.app.models.system_settings import settings
+from arches.app.utils import import_class_from_string
+
+
+class FileTemplateModifierFactory:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def get_file_template_modifier_class():
+        if settings.setting_exists("CONSULTATION_FILE_TEMPLATE_MODIFIER"):
+            return import_class_from_string(settings.CONSULTATION_FILE_TEMPLATE_MODIFIER)
+
+
+class FileTemplateModifier:
+    """
+    Base class for adding custom information to the Consultation file template.
+    """
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def get_template_path():
+        """
+        Adds custom Consultation letter template configuration
+
+        :return: dict containing uuid: consultation letter doc name
+        :rtype dict
+        """
+        pass
